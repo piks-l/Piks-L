ab5b6269869d7db7b8e5738c159f32481d5cf9d4
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-21 19:09:51 +0100

    Update index.vue

--- pages/artworks/index.vue
+++ pages/artworks/index.vue
@@ -1,22 +1,21 @@
 <template>
 <div>
   <div class="container border">
-    <h2>L'équipe</h2>
-    <p>Les artistes de <b>la Cour des Miracles</b> sont tous des <i>artistes</i> reconnus et confirmés. Ils ont chacun leur propre <i>style</i>, il est important de vous familiariser avec leurs différents <i>univers</i>. Nous vous proposons donc d’aller voir les <i>galeries</i> et <i>pages perso</i> de chacun et de suivre les liens proposés.</p>
+    <h2>ARTWORKS</h2>
   </div>
-  <cdm-artistes/> 
+  <cdm-artworks/>
 </div>
 </template>
 
 <script>
   import $ from 'jquery'
   // import components
-  import cdmArtistes from '~/components/artistes.vue'
+  import cdmArtworks from '~/components/artworks.vue'
   // export
   export default {
     layout: 'default',
     components: {
-        cdmArtistes
+        cdmArtworks
     },
     mounted: () => {
       	
