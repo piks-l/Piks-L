408cd22c61ec34f523c37bce016d6da3d41df02d
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-21 23:16:29 +0100

    Update index.vue

--- pages/tattoos/index.vue
+++ pages/tattoos/index.vue
@@ -4,19 +4,19 @@
     <h2>L'équipe</h2>
     <p>Les artistes de <b>la Cour des Miracles</b> sont tous des <i>artistes</i> reconnus et confirmés. Ils ont chacun leur propre <i>style</i>, il est important de vous familiariser avec leurs différents <i>univers</i>. Nous vous proposons donc d’aller voir les <i>galeries</i> et <i>pages perso</i> de chacun et de suivre les liens proposés.</p>
   </div>
-  <cdm-artistes/> 
+  <influx-tattoos/> 
 </div>
 </template>
 
 <script>
   import $ from 'jquery'
   // import components
-  import cdmArtistes from '~/components/artistes.vue'
+  import influxTattoos from '~/components/tattoos.vue'
   // export
   export default {
     layout: 'default',
     components: {
-        cdmArtistes
+        influxTattoos
     },
     mounted: () => {
       	
