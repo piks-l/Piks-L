c786c4f742f638fbced409ec648c20a28e83666c
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-23 16:58:15 +0100

    Update tattoos.vue

--- components/tattoos.vue
+++ components/tattoos.vue
@@ -15,7 +15,9 @@
 </template>
 
 <script>
+  import VueLazyload from 'vue-lazyload'
   export default {
+    components: { VueLazyload },
     data() {
       const context = require.context('~/content/tattoos/page/', false, /\.json$/);
       const tattoos = context.keys().map(key => ({
