4be629b19df26d5fc62e74cabd50e8cfe61fcfa7
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-27 10:58:27 +0100

    Update footer.vue

--- components/footer.vue
+++ components/footer.vue
@@ -1,14 +1,23 @@
 <template>
-    <footer id="footer">
-     <div class="topnav-centered">
-      <nuxt-link class="effect-left" to="/">Accueil</nuxt-link>
-      <nuxt-link class="no-active" to="/">Biographie</nuxt-link>
-      <nuxt-link class="no-active" to="/blog/">Actualités</nuxt-link>
-      <nuxt-link class="no-active" to="/artworks/">Artworks</nuxt-link>
-      <nuxt-link class="no-active" to="/tattoos/">Tattoos</nuxt-link>
-      <nuxt-link class="effect-right" to="/contact/">Contact</nuxt-link> 
-     </div>
-    </footer>
+  <footer id="footer">
+    <div class="navigation">
+      <nuxt-link to="/">Accueil</nuxt-link>
+      <nuxt-link to="/">Biographie</nuxt-link>
+      <nuxt-link to="/blog/">Actualités</nuxt-link>
+      <nuxt-link to="/artworks/">Artworks</nuxt-link>
+      <nuxt-link to="/tattoos/">Tattoos</nuxt-link>
+      <nuxt-link to="/contact/">Contact</nuxt-link> 
+    </div>
+    <div class="reseaux">
+      <a href="#"><i class="fab fa-facebook-f"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+      <a href="#"><i class="fab fa-youtube"></i></a>
+    </div>
+    <div class="copyright">
+      <p>© Copyright 2019 Sebatien-Odd - All Rights Reserved</p>
+    </div>
+  </footer>
 </template>
 <style>
 #footer {
