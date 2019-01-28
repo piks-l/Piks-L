cdb5227be68c2afbfb1e4bb50e7621ab1f015616
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-28 10:49:32 +0100

    Create header.vue

---
+++ components/ui/header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <img class="font-logo" src="images/uploads/logobombyx.png" alt="Logo Papillon">
+    <img class="font-logo" src="images/uploads/00000000050.png" alt="Logo Sebatien Odd Tattoo">
+  </header>
+</template>
+<style>
+  .header{
+    position: relative;
+    z-index: -1;
+    height:70vh;
+    width:100%;
+  }
+  .font-logo {
+      height: 100vh;
+      left: 50%;
+      position: fixed;
+      top: 50%;
+      -webkit-transform: translateX(-50%) translateY(-50%);
+      transform: translateX(-50%) translateY(-50%);
+  }
+</style>
+
+<script>
+//  import VueLazyload from 'vue-lazyload'
+//  export default { components: { VueLazyload }  };
+</script>
