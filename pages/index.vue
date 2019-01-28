640793833b5788f5f5b7d82cea702493a3b050fd
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-28 11:37:37 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,25 @@
 <template>
-<div>
-  <influx-shop/>
-  <div class="container border black">
-    <h2>Sébastien Odd, tatoueur on the road vivant à Toulouse.</h2>
-    <p>Il travaille plusieurs styles notamment le style réaliste pour lequel il est souvent demandé. Il adore aussi l’univers des comics, du jeu vidéo et du dessin en général et tatoue également beaucoup de pièces ,en newschool et en néo-trad, aux illustrations de qualité et au dessin maîtrisé.</p>
-  </div>
-  <div class="container border black">
-    <p><b>Pour aborder vos projets et prendre rendez-vous, veuillez m'envoyer un mail en précisant l'emplacement, les dimensions et l'inspirations en y joignant éventuellement 2 ou 3 photos</b></p>
-  </div>
-  
-  <div class="container border">
-    <h2>Actualités</h2>
-  </div>  
-  <div class="content-blog">
-    <influx-blog/>
-      <div class="full-width">
-        <div class="container border black">
-            <nuxt-link to="/blog/"><p>Voir les autres actualités !</p></nuxt-link>
-      </div>
-  </div>
-
-</div> 
-</div>
+  <section>
+    <influx-shop/>
+    <div class="container border black">
+      <h2>Sébastien Odd, tatoueur on the road vivant à Toulouse.</h2>
+      <p>Il travaille plusieurs styles notamment le style réaliste pour lequel il est souvent demandé. Il adore aussi l’univers des comics, du jeu vidéo et du dessin en général et tatoue également beaucoup de pièces ,en newschool et en néo-trad, aux illustrations de qualité et au dessin maîtrisé.</p>
+    </div>
+    <div class="container border black">
+      <p><b>Pour aborder vos projets et prendre rendez-vous, veuillez m'envoyer un mail en précisant l'emplacement, les dimensions et l'inspirations en y joignant éventuellement 2 ou 3 photos</b></p>
+    </div>
+    <div class="container border">
+      <h2>Actualités</h2>
+    </div>  
+    <div class="content-blog">
+      <influx-blog/>
+        <div class="full-width">
+          <div class="container border black">
+              <nuxt-link to="/blog/"><p>Voir les autres actualités !</p></nuxt-link>
+          </div>
+        </div>
+    </div> 
+  </section>
 </template>
 
 <script>
