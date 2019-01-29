3e7237f0c13d12cc64c85829fbf770c0e8437d28
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-29 13:52:56 +0100

    Update header.vue

--- components/ui/header.vue
+++ components/ui/header.vue
@@ -4,6 +4,21 @@
       <source src="https://www.influx-agence.fr/cms/sebastienodd-animation.mp4" type="video/mp4">
     </video>
     <div class="center-logo">
+      <h2>Simple Layout Change Prototype</h2>
+      <button @click="changed = !changed">Change</button>
+      <br>
+      <svg :class="{ active: changed }" xmlns="http://www.w3.org/2000/svg" width="447" height="442" viewBox="0 0 447 442">
+        <transition-group name="layout" tag="g">
+          <rect class="items rect" ref="rect" key="rect" width="171" height="171"/>
+          <circle class="items circ" key="circ" id="profile" cx="382" cy="203" r="65"/>
+          <g class="items text" id="text" key="text">
+            <rect x="56" y="225" width="226" height="16"/>
+            <rect x="56" y="252" width="226" height="16"/>
+            <rect x="56" y="280" width="226" height="16"/>
+          </g>
+          <rect class="items footer" key="footer" id="footer" y="423" width="155" height="19" rx="9.5" ry="9.5"/>
+        </transition-group>
+      </svg>
       <img src="images/uploads/00000000050.png" alt="Logo Sebatien Odd Tattoo">
     </div>
   </header>
@@ -47,9 +62,42 @@
       -webkit-transform: translate(-50%, -50%);
       transform: translate(-50%, -50%);
   }
+  
+  
+.items {
+  transition: all 0.4s ease;
+}
+
+#text {
+  transform-origin: 50% 50%;
+}
+
+svg {
+  fill: #A8DADC;
+}
+
+.active {
+  fill: #E63946;
+  .rect { 
+    transform: translate3d(0, 30px, 0); 
+  }
+  .circ {
+    transform: translate3d(30px, 0, 0) scale(0.5);
+  }
+  .text {
+    transform: rotate(90deg) scaleX(0.08) translate3d(-300px, -35px, 0);
+  }
+  .footer {
+    transform: translate3d(100px, 0, 0);
+  }
+}
 </style>
 
 <script>
 //  import VueLazyload from 'vue-lazyload'
-//  export default { components: { VueLazyload }  };
+export default { 
+  data() {
+    return { changed: false }
+  }
+};
 </script>
