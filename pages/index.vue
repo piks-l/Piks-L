285d917335a81c99e0944966dc950073642afb06
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-29 14:28:38 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,23 @@
 <template>
   <div>
     <section>
-      <div class="container border black">
+      <div class="container">
         <h2>Sébastien Odd, tatoueur on the road vivant à Toulouse.</h2>
         <p>Il travaille plusieurs styles notamment le style réaliste pour lequel il est souvent demandé. Il adore aussi l’univers des comics, du jeu vidéo et du dessin en général et tatoue également beaucoup de pièces ,en newschool et en néo-trad, aux illustrations de qualité et au dessin maîtrisé.</p>
       </div>
     </section>
     <section>
-      <div class="container border black">
+      <div class="container">
         <p><b>Pour aborder vos projets et prendre rendez-vous, veuillez m'envoyer un mail en précisant l'emplacement, les dimensions et l'inspirations en y joignant éventuellement 2 ou 3 photos</b></p>
       </div>
     </section>
     <section>
-      <div class="container border">
+      <div class="container">
         <h2>Actualités</h2>
-      </div>  
-    </section>
-    <section>
-      <div class="content-blog">
-        <div class="full-width">
-          <h2>Actualités</h2>
-        </div>
-        <influx-blog/>
-        <div class="full-width">
-          <div class="container border black">
-              <nuxt-link to="/blog/"><p>Voir les autres actualités !</p></nuxt-link>
-          </div>
-        </div>
+      </div>
+      <influx-blog/>
+      <div class="container">
+          <nuxt-link to="/blog/"><p>Voir les autres actualités !</p></nuxt-link>
       </div>
     </section>
   </div>
@@ -61,33 +52,7 @@
 </script>
 <style>
 /* DOUBLE BUTTON*/
-.choix-creation {
-  height: 100px;
-  display: block;
-  position: relative;
-}
-.split {
-  height: 100%;
-  width: 50%;
-  position: absolute;
-  z-index: 1;
-  overflow-x: hidden;
-}
-.gauche {
-  left: 0;
-  background-color: #252525;
-}
-.droite {
-  right: 0;
-  background-color: #000000;
-}
-.centre {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
+
 @media only screen and (min-width:960px) and (max-width:1264px){
 
 }
