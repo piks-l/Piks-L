9a2df7ba4a75b47ee82241056792a014f829ad2e
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-01-29 14:12:44 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,47 @@
 <template>
-  <section>
-    <div class="choix-creation">
-      <div class="split gauche">
-        <div class="centre">
-          <nuxt-link to="/artworks/"><h3>Artworks</h3></nuxt-link>
-        </div>
+  <div>
+    <section>
+      <div class="container border black">
+        <h2>Sébastien Odd, tatoueur on the road vivant à Toulouse.</h2>
+        <p>Il travaille plusieurs styles notamment le style réaliste pour lequel il est souvent demandé. Il adore aussi l’univers des comics, du jeu vidéo et du dessin en général et tatoue également beaucoup de pièces ,en newschool et en néo-trad, aux illustrations de qualité et au dessin maîtrisé.</p>
       </div>
-      <div class="split droite">
-        <div class="centre">
-          <nuxt-link to="/tattoos/"><h3>Tattoos</h3></nuxt-link>
-        </div>
+    </section>
+    <section>
+      <div class="container border black">
+        <p><b>Pour aborder vos projets et prendre rendez-vous, veuillez m'envoyer un mail en précisant l'emplacement, les dimensions et l'inspirations en y joignant éventuellement 2 ou 3 photos</b></p>
       </div>
-    </div>
-    <div class="container border black">
-      <h2>Sébastien Odd, tatoueur on the road vivant à Toulouse.</h2>
-      <p>Il travaille plusieurs styles notamment le style réaliste pour lequel il est souvent demandé. Il adore aussi l’univers des comics, du jeu vidéo et du dessin en général et tatoue également beaucoup de pièces ,en newschool et en néo-trad, aux illustrations de qualité et au dessin maîtrisé.</p>
-    </div>
-    <div class="container border black">
-      <p><b>Pour aborder vos projets et prendre rendez-vous, veuillez m'envoyer un mail en précisant l'emplacement, les dimensions et l'inspirations en y joignant éventuellement 2 ou 3 photos</b></p>
-    </div>
-    <div class="container border">
-      <h2>Actualités</h2>
-    </div>  
-    <div class="content-blog">
-      <div class="full-width">
+    </section>
+    <section>
+      <div class="container border">
         <h2>Actualités</h2>
-      </div>
-      <influx-blog/>
+      </div>  
+    </section>
+    <section>
+      <div class="content-blog">
+        <div class="full-width">
+          <h2>Actualités</h2>
+        </div>
+        <influx-blog/>
         <div class="full-width">
           <div class="container border black">
               <nuxt-link to="/blog/"><p>Voir les autres actualités !</p></nuxt-link>
           </div>
         </div>
-    </div> 
-  </section>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
   import $ from 'jquery'
   // import components
   import influxBlog from '~/components/blog.vue'
-  import influxShop from '~/components/mini-shop.vue'
 
   // export
   export default {
     layout: 'default',
     components: {
-        influxBlog,
-        influxShop
+        influxBlog
     },
     head() {
       return {
