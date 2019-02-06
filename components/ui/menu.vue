438badede17930d343460a81b6760cf82ba0752b
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-06 12:06:24 +0100

    Update menu.vue

--- components/ui/menu.vue
+++ components/ui/menu.vue
@@ -760,15 +760,7 @@
                     }, 0.07);
                 }
           }
-          let allowTilt = true;
-          const tilt = new TiltFx();
-          // Hovering the github link zooms in the main image.
-          const githubEl =  document.querySelector('.github');
-          githubEl.addEventListener('mouseenter', () => {
-              allowTilt = false;
-              tilt.zoom()
-          });
-          githubEl.addEventListener('mouseleave', () => allowTilt = true);
+
       }
     }
   }  
