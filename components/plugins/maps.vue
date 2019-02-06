c788e7a5e78a1dfc338f07b3ce629fd6084289b0
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-06 11:21:56 +0100

    Update maps.vue

--- components/plugins/maps.vue
+++ components/plugins/maps.vue
@@ -10,6 +10,15 @@
       return {
         mapName: this.name + "-map",
       }
+    },
+    mounted: function () {
+      const element = document.getElementById(this.mapName)
+      const options = {
+        zoom: 14,
+        center: new google.maps.LatLng(43.5993471,1.4431301)
+      }
+
+      const map = new google.maps.Map(element, options);
     }
   };
 </script>
