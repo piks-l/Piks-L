fcded355c65e1e238ee6dcbde72082cfdfb78f58
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-06 15:16:19 +0100

    Update menu.vue

--- components/ui/menu.vue
+++ components/ui/menu.vue
@@ -26,7 +26,7 @@
 							<nuxt-link class="mainmenu__item" to="/contact/">Contact</nuxt-link>
 						</div>
 						<p class="label label--topleft label--vert-mirror">menu</p>
-						<p class="label label--bottomright label--vert">www.sebastien-odd.tattoo/<strong>{{this.$route.path}}</strong></p>
+						<p class="label label--bottomright label--vert">www.sebastien-odd.tattoo<strong>{{this.$route.path}}</strong></p>
 					</div>
 				</div>
 				<div class="menu__item menu__item--2" data-direction="lr">
