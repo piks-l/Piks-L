aab447af0692b97c7a2abb096e8fa908e5495306
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-06 11:09:04 +0100

    Update maps.vue

--- components/plugins/maps.vue
+++ components/plugins/maps.vue
@@ -1,1 +1,24 @@
+<template>
+  <div class="google-map" :id="mapName"></div>
+</template>
 
+<script>
+  export default {
+    name: 'google-map',
+    props: ['name'],
+    data: function () {
+      return {
+        mapName: this.name + "-map",
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .google-map {
+    width: 800px;
+    height: 600px;
+    margin: 0 auto;
+    background: gray;
+  }
+</style>
