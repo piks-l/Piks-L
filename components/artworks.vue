5583183d466a8bb0bbf5dd9cb72123ce4839e7d2
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-06 12:41:07 +0100

    Update artworks.vue

--- components/artworks.vue
+++ components/artworks.vue
@@ -2,7 +2,7 @@
     <div class="equipes">
       <div class="container">
           <div class="wrapper">
-              <article v-for="artwork in artworks.slice().reverse().slice(0,4)" :key="artwork.date">  
+              <article v-for="artwork in artworks.slice().reverse().slice(0,4)" :key="artwork.date" :class="{ active: openWork }" @click="openWork = true">  
                   <nuxt-link class="title" :to="artwork._path">
                     <div class="thumb">
                         <img  v-lazy="artwork.thumbnail" :alt="artwork.title">
@@ -24,7 +24,10 @@
         ...context(key),
         _path: `/artworks/${key.replace('.json', '').replace('./', '')}`
       }));
-      return { artworks };
+      return { 
+        openWork: false,
+        artworks 
+      };
     }
   };
 </script>
