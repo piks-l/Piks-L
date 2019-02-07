76000e01a68e06ca3fb7e3d67b137c421770069d
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-07 11:42:29 +0100

    Update tattoos.vue

--- components/tattoos.vue
+++ components/tattoos.vue
@@ -1,16 +1,13 @@
 <template>
-    <div class="equipes">
-      <div class="container">
-          <div class="wrapper">
-              <article v-for="tattoo in tattoos.slice().reverse().slice(0,4)" :key="tattoo.date">  
-                  <nuxt-link class="title" :to="tattoo._path">
-                    <div class="thumb">
-                        <img v-lazy="tattoo.thumbnail" :alt="tattoo.title">
-                    </div>
-                  </nuxt-link>
-              </article>
-          </div>
-      </div>
+    <div class="portfolio">
+       <article v-for="tattoo in tattoos.slice().reverse()" :key="tattoo.date" class="works" :class="{ active: openWork }" @click="openWork = true">
+          <nuxt-link class="title" :to="artwork._path">
+            <div class="center" >
+              <img class="fontblur" v-lazy="tattoo.thumbnail" :alt="tattoo.title">
+              <img class="artists-img" v-lazy="tattoo.thumbnail" :alt="tattoo.title">
+            </div>  
+          </nuxt-link>
+       </article>
     </div>
 </template>
 
@@ -28,3 +25,49 @@
     }
   };
 </script>
+<style>
+  .portfolio {
+      position: relative;
+  }
+  .works {
+      width: 25%;
+      display: inline-block;
+      height: 25vw;
+      overflow: hidden;
+      margin-bottom: -5px;
+      position: relative;
+  }
+  .center {
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      position: relative;
+  }
+  .artists-img {
+      position: relative;
+      top: 50%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      filter:blur(50px);
+      -webkit-transition: 0.5s all ease;
+      transition: 0.5s all ease;
+      width: auto;
+      height: auto;
+      max-width: 100%;
+      max-height: 100%;
+  }
+  .artists-img[lazy=loaded]{
+      filter:blur(0px) !important;
+  }
+  .fontblur{
+      top: 50%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      filter:blur(50px);
+      position: absolute;
+      height:110%;
+      width: auto;
+  }
+</style>
