77e8598b436f23d1b54d19cd8b6b39c88a74c953
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-07 11:39:34 +0100

    Update index.vue

--- pages/tattoos/index.vue
+++ pages/tattoos/index.vue
@@ -1,8 +1,7 @@
 <template>
 <div>
   <div class="container border">
-    <h2>L'équipe</h2>
-    <p>Les artistes de <b>la Cour des Miracles</b> sont tous des <i>artistes</i> reconnus et confirmés. Ils ont chacun leur propre <i>style</i>, il est important de vous familiariser avec leurs différents <i>univers</i>. Nous vous proposons donc d’aller voir les <i>galeries</i> et <i>pages perso</i> de chacun et de suivre les liens proposés.</p>
+    <h2>TATTOOS</h2>
   </div>
   <influx-tattoos/> 
 </div>
