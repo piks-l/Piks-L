9117a62e7e795e0b9ab79f87445f6fd9b5192c9f
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-08 13:38:11 +0100

    Update studios.vue

--- pages/studios.vue
+++ pages/studios.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="container border black">
+    <div class="container">
       <h2>Nos studios</h2>
       <p>Pour chaque projet nous préférons bien évidemment en discuter avec vous directement. La plupart des tatouages réalisés sont des motifs originaux, et une rencontre est souvent nécessaire. Cependant pour un premier contact ou ceux qui viennent de loin il est possible de contacter par <a href="mailto:contact@lacourdesmiracles.tattoo">mail</a> ou via notre page facebook.</p>
     </div>
     <cdm-shop/>
-      <div class="container border black">
+      <div class="container">
       <p><b>Tous nos locaux respectent des règles d’hygiène strictes tant au niveau de la stérilisation qu’au niveau des matériaux utilisés !</b></p>
     </div>  
   </div>
@@ -13,14 +13,9 @@
 
 <script>
   import $ from 'jquery'
-  // import components
-  import cdmShop from '~/components/shop.vue'
   // export
   export default {
     layout: 'default',
-    components: {
-        cdmShop
-    },
     head() {
       return {
         title: 'LES STUDIOS',
@@ -33,21 +28,10 @@
       }
     },
     mounted () {
-      	VanillaTilt.init(document.querySelectorAll("h2, .street-views"), {
-          max: 25,
-          speed: 400
-        });
+
     }
   }
 </script>
 <style>
-@media only screen and (min-width:960px) and (max-width:1264px){
 
-}
-@media only screen and (min-width:600px) and (max-width:959px){
-
-}
-@media only screen and (max-width:599px){
-
-}
 </style>
