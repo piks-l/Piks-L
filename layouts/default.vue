2feabdcbfdd93ca38b55b92717591b67079b1d8b
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-08 13:19:02 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
+  <div class="layout-influx">
     <influx-menu/>
     <influx-header/>
     <nuxt/>
     <influx-footer/>
+    <influx-cursor/>
   </div>
 </template>
 <script>
@@ -11,18 +12,17 @@
   import influxMenu from '~/components/ui/menu.vue'
   import influxHeader from '~/components/ui/header.vue'
   import influxFooter from '~/components/ui/footer.vue'
+  import influxCursor from '~/components/ui/cursor.vue'
   export default {
       transition: 'page',
       components: {
           influxMenu,
           influxHeader,
-          influxFooter
+          influxFooter,
+          influxCursor
       },
       mounted: () => {
           
       }
   }
 </script>
-<style>
-
-</style>
