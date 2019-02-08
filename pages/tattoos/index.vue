64e8931aefbc40af440af1e8be17f6ebe92c83a1
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-08 14:10:25 +0100

    Update index.vue

--- pages/tattoos/index.vue
+++ pages/tattoos/index.vue
@@ -3,19 +3,34 @@
   <div class="container border">
     <h2>TATTOOS</h2>
   </div>
-  <influx-tattoos/> 
+  <div class="portfolio">
+     <article v-for="tattoo in tattoos.slice().reverse()" :key="tattoo.date" class="works">
+        <nuxt-link class="title" :to="tattoo._path">
+          <div class="centrer" >
+            <img class="fontblur" v-lazy="tattoo.thumbnail" :alt="tattoo.title">
+            <img class="artists-img" v-lazy="tattoo.thumbnail" :alt="tattoo.title">
+          </div>  
+        </nuxt-link>
+     </article>
+  </div>
 </div>
 </template>
 
 <script>
   import $ from 'jquery'
-  // import components
-  import influxTattoos from '~/components/tattoos.vue'
+  import VueLazyload from 'vue-lazyload
+
   // export
   export default {
     layout: 'default',
-    components: {
-        influxTattoos
+    components: { VueLazyload },
+    data() {
+      const context = require.context('~/content/tattoos/page/', false, /\.json$/);
+      const tattoos = context.keys().map(key => ({
+        ...context(key),
+        _path: `/tattoos/${key.replace('.json', '').replace('./', '')}`
+      }));
+      return { tattoos };
     },
     mounted: () => {
       	
