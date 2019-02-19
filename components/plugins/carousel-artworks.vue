94c02383bce4013b6e717b1cdba95f8f6b104e48
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-19 22:11:48 +0100

    Update carousel-artworks.vue

--- components/plugins/carousel-artworks.vue
+++ components/plugins/carousel-artworks.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="carousel">
-    <div class="carousel__nav">
-     <span id="moveLeft" class="carousel__arrow">
-          <svg class="carousel__icon" width="24" height="24" viewBox="0 0 24 24">
-      <path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"></path>
-  </svg>
+  <div class="carousel-artworks">
+    <div class="carousel-artworks__nav">
+      <span id="moveLeft-artworks" class="carousel-artworks__arrow">
+        <svg class="carousel__icon" width="24" height="24" viewBox="0 0 24 24">
+          <path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"></path>
+        </svg>
       </span>
-      <span id="moveRight" class="carousel__arrow" >
+      <span id="moveRight-artworks" class="carousel-artworks__arrow" >
         <svg class="carousel__icon"  width="24" height="24" viewBox="0 0 24 24">
-    <path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path>
-  </svg>    
+          <path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path>
+        </svg>    
       </span>
-    </div>
-   <div v-for="art in arts.slice().reverse().slice(0, 3)" :key="art.date" class="carousel-item carousel-item--1">
-
+     </div>
+     <div v-for="art in arts.slice().reverse().slice(0, 3)" :key="art.date" class="carousel-item-artworks">
       <div class="carousel-item__info">
-        <div class="carousel-item__container">
-        <h2 class="carousel-item__subtitle">{{ art.date }}</h2>
-        <h1 class="carousel-item__title">{{ art.title }}</h1>
-        <nuxt-link :to="art._path" class="carousel-item__btn">En savoir plus</nuxt-link>
-          </div>
+      <div class="carousel-item__container">
+      <h2 class="carousel-item__subtitle">{{ art.date }}</h2>
+      <h1 class="carousel-item__title">{{ art.title }}</h1>
+      <nuxt-link :to="art._path" class="carousel-item__btn">En savoir plus</nuxt-link>
       </div>
-            <div class="carousel-item__image" :style="{ backgroundImage: `url(${art.couverture})` }"></div>
+      </div>
+      <div class="carousel-item__image" :style="{ backgroundImage: `url(${art.couverture})` }"></div>
     </div>
-
   </div>
 </template>
 <script>
@@ -37,33 +35,33 @@
       return { arts };
     },
     mounted: function () {
-      $('.carousel-item').eq(0).addClass('active');
-      var total = $('.carousel-item').length;
-      var current = 0;
-      $('#moveRight').on('click', function(){
-        var next=current;
-        current= current+1;
-        setSlide(next, current);
+      $('.carousel-item-artworks').eq(0).addClass('active');
+      var total = $('.carousel-item-artworks').length;
+      var current-artworks = 0;
+      $('#moveRight-artworks').on('click', function(){
+        var next=current-artworks;
+        current-artworks= current-artworks+1;
+        setSlide(next, current-artworks);
       });
-      $('#moveLeft').on('click', function(){
-        var prev=current;
-        current = current- 1;
-        setSlide(prev, current);
+      $('#moveLeft-artworks').on('click', function(){
+        var prev=curren-artworkst;
+        current-artworks = current-artworks- 1;
+        setSlide(prev, current-artworks);
       });
       function setSlide(prev, next){
-        var slide= current;
+        var slide= current-artworks;
         if(next>total-1){
          slide=0;
-          current=0;
+          current-artworks=0;
         }
         if(next<0){
           slide=total - 1;
-          current=total - 1;
+          current-artworks=total - 1;
         }
-       $('.carousel-item').eq(prev).removeClass('active');
-       $('.carousel-item').eq(slide).addClass('active');
+       $('.carousel-item-artworks').eq(prev).removeClass('active');
+       $('.carousel-item-artworks').eq(slide).addClass('active');
         setTimeout(function(){},800);
-        console.log('current '+current);
+        console.log('current '+current-artworks);
         console.log('prev '+prev);
       }
     }
@@ -73,7 +71,7 @@
 .caroublog {
   height: 350px!important;
 }
-.carousel {
+.carousel-artworks {
     width: 100%;
     height: 100%;
     display: flex;   
@@ -81,7 +79,7 @@
   position: relative;
 }
 
-.carousel-item {
+.carousel-item-artworks {
   visibility:visible;
     display: flex;
     width: 100%;
