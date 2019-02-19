dba089d730fb2b283b8e951f9b61195ceffd33e1
INFLUX AGENCE <44368624+InfluxAgence@users.noreply.github.com>
2019-02-19 22:23:53 +0100

    Update carousel-artworks.vue

--- components/plugins/carousel-artworks.vue
+++ components/plugins/carousel-artworks.vue
@@ -6,7 +6,7 @@
           <path d="M20,11V13H8L13.5,18.5L12.08,19.92L4.16,12L12.08,4.08L13.5,5.5L8,11H20Z"></path>
         </svg>
       </span>
-      <span id="moveRight-artworks" class="carousel-artworks__arrow" >
+      <span id="moveRight-artworks" class="carousel__arrow" >
         <svg class="carousel__icon"  width="24" height="24" viewBox="0 0 24 24">
           <path d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path>
         </svg>    
