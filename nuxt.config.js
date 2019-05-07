27fc57d563200bc091deebf05a0012cf54d9d09c
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-07 18:54:01 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ module.exports = {
       { 'property': 'og:image', 'content': 'images/uploads/link_share.jpg', 'vmid': 'og:image' }
     ],
     link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+      { rel: 'shortcut icon', href: '/favicon.ico' },
       { rel: 'stylesheet', integrity: 'sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz', href: 'https://use.fontawesome.com/releases/v5.4.1/css/all.css', crossorigin: 'anonymous' }
     ],
     script: [
