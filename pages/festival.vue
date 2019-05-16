199512e8d4ff174f3a87130ee873fae8cdd5b3d5
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-16 14:57:16 +0200

    Create festival.vue

---
+++ pages/festival.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page">
+    <h2>PROGRAMMATION</h2>
+  </div>
+</template>
+<script>
+  import $ from 'jquery'
+  // export
+  export default {
+    layout: 'default',
+    components: { },
+    head() {
+      return {
+        title: 'Electrobotik Invasion - le 2 & 3 Août 2019',
+        meta: [
+          { hid: 'description', name: 'description', content: 'Electrobotik Invasion Festival, le 2 & 3 Août 2019, Circuit Paul Ricard - Le Castelet (83).' },
+          { 'property': 'og:title', 'content': 'Electrobotik Invasion - le 2 & 3 Août 2019', 'vmid': 'og:title' },
+          { 'property': 'og:description', 'content': 'Electrobotik Invasion Festival, le 2 & 3 Août 2019, Circuit Paul Ricard - Le Castelet (83).' },
+          { 'property': 'og:image', 'content': 'images/uploads/link_share.jpg', 'vmid': 'og:image' }
+        ]
+      }
+    },
+    mounted () {	
+    }
+  }
+</script>
+<style>
+  @media only screen and (min-width:960px) and (max-width:1264px){}
+  @media only screen and (min-width:600px) and (max-width:959px){}
+  @media only screen and (max-width:599px){}
+</style>
