0a2416c84c75e840d4b262e84d808ba74763dc48
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-16 11:07:46 +0200

    Update menu.vue

--- components/ui/menu.vue
+++ components/ui/menu.vue
@@ -55,28 +55,7 @@
   import $ from 'jquery'
   export default {
     mounted: () => {
-      $( ".open-menu" ).click(function() {
-          $( ".menu" ).addClass( "open" );
-        });
-        $( ".close" ).click(function() {
-          $( ".menu" ).removeClass( "open" );
-        });
-
-        if(document.getElementById("header") === null){}else{
-            window.onscroll = function() {scrollfunction()};
-            var header = document.getElementById("header");
-            var logo = document.getElementById("logo");
-            var sticky = header.offsetHeight;
-            function scrollfunction() {
-                if (window.pageYOffset > sticky) {
-                  header.classList.add("sticky");
-                  logo.classList.add("grab");
-                } else {
-                  header.classList.remove("sticky");
-                  logo.classList.remove("grab");
-                }
-            }
-        }
+     
     } // Mounted end
   } // Export end 
 </script>
