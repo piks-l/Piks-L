ccea67fb3edd1780682c3b090d19d99f3e902735
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-16 12:53:38 +0200

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -1,86 +1,44 @@
-$(function() {
+$(function () {
+  // Menu
+  $( ".open-menu" ).click(function() { $( ".menu" ).addClass( "open" ); });
+  $( ".close" ).click(function() { $( ".menu" ).removeClass( "open" ); });
+  if(document.getElementById("header") === null){}else{
+      window.onscroll = function() {scrollfunction()};
+      var header = document.getElementById("header");
+      var logo = document.getElementById("logo");
+      var sticky = header.offsetHeight;
+      function scrollfunction() {
+          if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+            logo.classList.add("grab");
+          } else {
+            header.classList.remove("sticky");
+            logo.classList.remove("grab");
+          }
+      }
+  }
+  console.log('document ready!')
+  // Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+  });
+  // Filter button
+  $('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  $('#filters2').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+  // Change is-checked class on buttons
+  $('.button-date, .button-stage').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'button', function() {
+          $buttonGroup.find('.is-checked').removeClass('is-checked');
+          $( this ).addClass('is-checked');
 
-	var marker = [], marker2 = [], infowindow = [], maps = [];
-
-	function addMarker(location,name,contentstr,map){
-        marker[name] = new google.maps.Marker({
-            position: location,
-            map: map,
-			icon: 'assets/img/marker.png',
-        });
-        marker[name].setMap(map);
-
-		
-		infowindow[name] = new google.maps.InfoWindow({
-			content: '<div class="scrollfix">'+contentstr+'</div>'
-		});
-		
-		google.maps.event.addListener(marker[name], 'click', function() {
-			infowindow[name].open(map,marker[name]);
-		});
-
-    }
-
-    function moveto_region(location,zoom_level) {
-		map.setZoom(zoom_level);
-		map.setCenter(location);
-		$('body, html').animate({'scrollTop': $('#map-canvas').offset().top - $('header').outerHeight()});
-	}
-
-	$('.marker').on('click', function(){
-		var coord1 = $(this).attr('data-lng');
-		var coord2 = $(this).attr('data-lat');
-		var location = new google.maps.LatLng(coord2,coord1);
-		moveto_region(location, 18);
-	});
-
-	function initialize(mapInst, index) {
-
-		var lat = mapInst.attr("data-lat");
-		var lng = mapInst.attr("data-lng");
-
-		var myLatlng = new google.maps.LatLng(lat,lng);
-
-		var setZoom = parseInt(mapInst.attr("data-zoom"));
-
-		var styles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
-		var styledMap = new google.maps.StyledMapType(styles,{name: "Styled Map"});
-
-		var mapOptions = {
-			zoom: setZoom,
-
-			scrollwheel: false,
-			
-			center: myLatlng,
-
-			disableDefaultUI: true
-		
-		};
-		maps[index] = new google.maps.Map(document.getElementById(mapInst.attr('id')), mapOptions);
-		
-		maps[index].mapTypes.set('map_style', styledMap);
-  		maps[index].setMapTypeId('map_style');
-		
-
-		$('.addresses-block[data-rel="'+mapInst.attr('id')+'"] a.marker').each(function(){
-			var mark_lat = $(this).attr('data-lat');
-			var mark_lng = $(this).attr('data-lng');
-			var this_index = $('.addresses-block a').index(this);
-			var mark_name = 'template_marker_'+this_index;
-			var mark_locat = new google.maps.LatLng(mark_lat, mark_lng);
-			var mark_str = $(this).attr('data-string');
-			addMarker(mark_locat,mark_name,mark_str,maps[index]);	
-		});
-	}
-
-	$(window).load(function(){
-		setTimeout(function(){
-			var index = 0;
-			$('.map-inst').each(function(){
-				initialize($(this), index);
-				index = index + 1;
-			});
-		}, 500);
-	});
-
-});
+      });
+  });
+})
