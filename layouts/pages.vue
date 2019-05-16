36f42bc804c951b8b2d6d9e1f7e7dcbbb018955d
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-16 15:25:17 +0200

    Create pages.vue

---
+++ layouts/pages.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout-electrobotik">
+    <electrobotik-menu/>
+    <nuxt/>
+    <electrobotik-footer/>
+  </div>
+</template>
+<script>
+  import $ from 'jquery'
+  import electrobotikMenu from '~/components/ui/menu.vue'
+  import electrobotikFooter from '~/components/ui/footer.vue'
+  export default {
+      transition: 'page',
+      components: {
+          electrobotikMenu,
+          electrobotikFooter
+      },
+      mounted: () => {
+      }
+  }
+</script>
