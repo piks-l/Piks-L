3856e7177550a82c68bd0e389c5f999ec714855f
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-16 14:42:45 +0200

    Update header.vue

--- components/ui/header.vue
+++ components/ui/header.vue
@@ -9,140 +9,6 @@
     </div>
   </header>
 </template>
-<style>
-#mouse_body {
-  border-style: solid;
-  border-width: 1px;
-  border-color: #525252;
-  border-radius: 32px;
-  /* used just to add space from top */
-  /* size of the mouse body */
-  height: 43px;
-  width: 23px;
-   /* to center in browser */
-  margin: 0 auto;
-  position: absolute;
-  bottom: 0;
-  left: 50%;
-  -webkit-transform: translatex(-50%);
-  transform: translatex(-50%);
-}
-.smog{
-    background:rgba(0,0,0,.5);
-    position:absolute;
-    width:100%;
-    height:100vh;
-    top:0;
-    left:0;
-}
-
-#mouse_wheel {
-  border-style: solid;
-  border-width: 1px;
-  border-color: #FFB413;
-  border-radius: 8px;
-  background-color: #fff;
-  /* position property required to play animation  */
-  position: relative; 
-  height: 3px;
-  width: 3px;
-  /* to center wheel in mouse body */
-  margin: 0 auto; 
-  animation: wheel_animation 1.5s linear infinite;
-}
-
-/* include use -moz-, -webkit-, or -o- for respective browser type*/
-@keyframes wheel_animation { 
-  0% {
-    opacity: 0;
-    top: 2px;
-  }
-  50% {
-    opacity: 1;
-    top: 50%;
-  }
-  100% {
-    opacity: 0;
-    top: 33px;
-  }
-}
-
-  video#animation {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      min-width: 100%;
-      min-height: 100%;
-      transform: translate(-50%, -50%);
-      transition:1s all ease;
-  }
-  .header{
-    position: relative;
-    height:80vh;
-    width:100%;
-    overflow: hidden;
-    transition:1s all ease;
-    z-index:0;
-  }
-  .header.active{
-    height:20vh;
-  }
-  .header.active #animation{
-    filter:blur(10px);
-    opacity:.5;
-  }
-  .logo {
-      height: auto;
-      left: 50%;
-      position: absolute;
-      top: 50%;
-      -webkit-transform: translateX(-50%) translateY(-50%);
-      transform: translateX(-50%) translateY(-50%);
-      max-width: 100%;
-      max-height: 100%;
-  }
-  .center-logo{
-      position: fixed;
-      width: 100%;
-      height: 80vh;
-  }
-
-.items {
-  transition: all 0.4s ease;
-}
-
-#text {
-  -webkit-transform-origin: 50% 50%;
-          transform-origin: 50% 50%;
-}
-
-svg {
-  fill: #A8DADC;
-}
-
-.active {
-  fill: #E63946;
-}
-.active .rect {
-  -webkit-transform: translate3d(0, 30px, 0);
-          transform: translate3d(0, 30px, 0);
-}
-.active .circ {
-  -webkit-transform: translate3d(30px, 0, 0) scale(0.5);
-          transform: translate3d(30px, 0, 0) scale(0.5);
-}
-.active .text {
-  -webkit-transform: rotate(90deg) scaleX(0.08) translate3d(-300px, -35px, 0);
-          transform: rotate(90deg) scaleX(0.08) translate3d(-300px, -35px, 0);
-}
-.active .footer {
-  -webkit-transform: translate3d(100px, 0, 0);
-          transform: translate3d(100px, 0, 0);
-}
-
-</style>
-
-
 <script>
 export default {
   data() {
@@ -152,3 +18,14 @@ export default {
   }
 }
 </script>
+<style>
+  video#animation {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      min-width: 100%;
+      min-height: 100%;
+      transform: translate(-50%, -50%);
+      transition:1s all ease;
+  }
+</style>
