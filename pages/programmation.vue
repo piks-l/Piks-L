0d299539d2d79ecd6c9de3749b337c8e79205aab
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-17 19:41:23 +0200

    Update programmation.vue

--- pages/programmation.vue
+++ pages/programmation.vue
@@ -150,7 +150,7 @@
             var filterGroup = $buttonGroup.attr('data-filter-group');
             // set filter for group
             filters[ filterGroup ] = $button.attr('data-filter');
-            filterss = $( event.currentTarget ).attr('data-filter');
+            var filterss = $(this).attr('data-filter');
             // combine filters
             var filterValue = concatValues( filters );
             // flatten object by concatting values
@@ -165,6 +165,7 @@
               filter: filterValue
             });
             console.log(filterss);
+             
     }
   }
   }
