12891a545d4f020f2ff70168d674451a147e4567
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-17 14:25:26 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,49 +1,49 @@
 $(function () {
   // Menu
   $( ".button-date button" ).click(function() {
-              $( ".button-stage .any" ).click();
-              $('.button-stage button, .other-stage button').each( function() {
-                  $( this ).addClass( "active" );
-                  $( ".button-date" ).removeClass( "mobile-active" );
-              });
-            });
-            $( ".button-stage button, .other-stage button" ).click(function() {
-              $('.button-stage button, .other-stage button').each( function() {
-                  $( this ).removeClass( "active" );
-                  $( ".stage" ).removeClass( "mobile-active" );
-              });
-            });
-            $( ".select-date" ).click(function() {
-              $( ".button-date" ).toggleClass( "mobile-active" );
-              $( ".stage" ).removeClass( "mobile-active" );
-            });
-            $( ".select-stage" ).click(function() {
-              $( ".stage" ).toggleClass( "mobile-active" );
-              $( ".button-date" ).removeClass( "mobile-active" );
-            });
+    $( ".button-stage .any" ).click();
+    $('.button-stage button, .other-stage button').each( function() {
+        $( this ).addClass( "active" );
+        $( ".button-date" ).removeClass( "mobile-active" );
+    });
+  });
+  $( ".button-stage button, .other-stage button" ).click(function() {
+    $('.button-stage button, .other-stage button').each( function() {
+        $( this ).removeClass( "active" );
+        $( ".stage" ).removeClass( "mobile-active" );
+    });
+  });
+  $( ".select-date" ).click(function() {
+    $( ".button-date" ).toggleClass( "mobile-active" );
+    $( ".stage" ).removeClass( "mobile-active" );
+  });
+  $( ".select-stage" ).click(function() {
+    $( ".stage" ).toggleClass( "mobile-active" );
+    $( ".button-date" ).removeClass( "mobile-active" );
+  });
 
-            $( ".open-menu" ).click(function() {
-              $( ".menu" ).addClass( "open" );
-            });
-            $( ".close, .menu-link" ).click(function() {
-              $( ".menu" ).removeClass( "open" );
-            });
-            if ($(window).width() > 599) {
-              if(document.getElementById("header") === null){}else{
-                  window.onscroll = function() {scrollfunction()};
-                  var header = document.getElementById("header");
-                  var logo = document.getElementById("sticky-logo");
-                  var sticky = header.offsetHeight;
-                  function scrollfunction() {
-                      if (window.pageYOffset > sticky) {
-                        header.classList.add("sticky");
-                        logo.classList.add("grab");
-                      } else {
-                        header.classList.remove("sticky");
-                        logo.classList.remove("grab");
-                      }
-                  }
-              }
+  $( ".open-menu" ).click(function() {
+    $( ".menu" ).addClass( "open" );
+  });
+  $( ".close, .menu-link" ).click(function() {
+    $( ".menu" ).removeClass( "open" );
+  });
+  if ($(window).width() > 599) {
+    if(document.getElementById("header") === null){}else{
+        window.onscroll = function() {scrollfunction()};
+        var header = document.getElementById("header");
+        var logo = document.getElementById("sticky-logo");
+        var sticky = header.offsetHeight;
+        function scrollfunction() {
+            if (window.pageYOffset > sticky) {
+              header.classList.add("sticky");
+              logo.classList.add("grab");
+            } else {
+              header.classList.remove("sticky");
+              logo.classList.remove("grab");
             }
+        }
+    }
+  }
   console.log('Main.js ready!')
 })
