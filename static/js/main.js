f2eb4efb0cbb8ca6e7393e381b511fbb4c444e30
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-17 12:38:56 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -17,27 +17,5 @@ $(function () {
           }
       }
   }
-  console.log('document ready!')
-  // Isotope
-  var $grid = $('.grid').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
-  });
-  // Filter button
-  $('#filters').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
-  $('#filters2').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
-  // Change is-checked class on buttons
-  $('.button-date, .button-stage').each( function( i, buttonGroup ) {
-      var $buttonGroup = $( buttonGroup );
-      $buttonGroup.on( 'click', 'button', function() {
-          $buttonGroup.find('.is-checked').removeClass('is-checked');
-          $( this ).addClass('is-checked');
-      });
-  });
+  console.log('Main.js ready!')
 })
