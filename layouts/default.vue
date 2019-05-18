56c249a371b4aad13b92031b068cf02351395d66
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-18 17:31:03 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="layout-electrobotik">
     <electrobotik-menu/>
-    <electrobotik-header/>
     <nuxt/>
   </div>
 </template>
 <script>
   import $ from 'jquery'
   import electrobotikMenu from '~/components/ui/menu.vue'
-  import electrobotikHeader from '~/components/ui/header.vue'
-  import electrobotikFooter from '~/components/ui/footer.vue'
   export default {
       transition: 'page',
       components: {
-          electrobotikMenu,
-          electrobotikHeader
+          electrobotikMenu
       },
       mounted: () => {
       }
