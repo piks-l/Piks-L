6d2b73ce2fc5c565720963398fb111e1083bd310
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-18 13:57:08 +0200

    Update _slug.vue

--- pages/artistes/_slug.vue
+++ pages/artistes/_slug.vue
@@ -1,16 +1,46 @@
-<template>
+<template> 
   <div class="page artist">
-    <div :class="'intro artist-'+title">
-      <div class="header">
-          <h2>{{ title }}</h2>    
-          <vue-markdown>{{description}}</vue-markdown>
-      </div>
+    <div class="decoration">
+      <img class="fond" src="../assets/images/scene-electro.jpg"  alt="pathern">
+      <div class="volet-artist"></div>
     </div>
-    <div class="full-width">
-        <div class="container">
-            <nuxt-link to="/tattoos/"><p>Voir les autres tattoos.</p></nuxt-link>
+    <div id="main-container" class="artiste">
+      <div  class="container scene_element scene_element--fadein ">
+
+        <nuxt-link to="/" class="fake-close">
+            <div class="left"></div>
+            <div class="right"></div>
+        </nuxt-link>
+
+        <div class="title">
+            <div class="background"></div>
+            <h2>{{ title }}</h2>
         </div>
-    </div>   
+        <div class="content-artiste">
+            <div class="vertical-align">
+                <div class="artiste-stage">
+                    <p>{{ stage }}</p>
+                </div>
+                <div class="artiste-date">
+                    <p>{{ dateshows }}</p>
+                </div>
+                <div class="content">
+                  <vue-markdown>{{description}}</vue-markdown>
+                </div>
+                <div class="social-artiste">
+                  <a href="#" alt="FB"><i class="fab fa-facebook-f"></i></a>
+                  <a href="#" alt="IT"><i class="fab fa-instagram"></i></a>
+                  <a href="#" alt="YT"><i class="fab fa-youtube"></i></a>
+                  <a href="#" alt="link"><i class="fas fa-link"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="video">
+            <vue-markdown>{{iframe}}</vue-markdown>
+        </div>
+
+      </div>
+    </div>  
   </div>
 </template>
 <script>
