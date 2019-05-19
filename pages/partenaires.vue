9786c3ec3f06d9caee9922813a8ba8eef41da4d1
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-19 15:33:12 +0200

    Update partenaires.vue

--- pages/partenaires.vue
+++ pages/partenaires.vue
@@ -6,7 +6,8 @@
             <div class="partenaire">
                 <div class="effect">
                     <div class="volet1">
-                        <img class="lazy" data-src="https://electrobotik.netlify.com/images/logos/2.png" >
+                        <img class="lazy" v-lazy="https://electrobotik.netlify.com/images/logos/1.png" >
+                        <img class="lazy" data-src="https://electrobotik.netlify.com/images/logos/1.png" >
                     </div>
                     <div class="volet2"></div>
                 </div>
