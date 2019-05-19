3747fe298db3c174687885a1791064fa9243c1de
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-19 15:50:17 +0200

    Update infos-pratiques.vue

--- pages/infos-pratiques.vue
+++ pages/infos-pratiques.vue
@@ -446,7 +446,22 @@
       }
     },
     mounted () {
-      	
+              // menu page info pratiques
+            $( ".select-infos button" ).click(function() {
+              var info =   $( this ).attr('data-class');
+              $('.select-fond').each( function() {
+                  $( this ).removeClass( "active" );
+              });
+              $('.slide').each( function() {
+                  $( this ).removeClass( "active" );
+              });
+              $( "." + info ).addClass( "active" );
+              $('.select-infos button').each( function() {
+                  $( this ).removeClass( "is-checked" );
+              });
+              $( this ).addClass( "is-checked" );
+              $('.content-large').removeAttr( "id" ).attr('id', info);
+});    	
     }
   }
 </script>
