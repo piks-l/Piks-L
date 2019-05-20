b26565667ebc7b2930aec90f6bf1dc233110de90
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-20 15:29:53 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -49,25 +49,5 @@ $(function () {
               $( ".menu" ).removeClass( "open" );
             });
 
-
-
-            // sticky header
-            if ($(window).width() > 599) {
-              if(document.getElementById("header") === null){}else{
-                  window.onscroll = function() {scrollfunction()};
-                  var header = document.getElementById("header");
-                  var logo = document.getElementById("sticky-logo");
-                  var sticky = header.offsetHeight;
-                  function scrollfunction() {
-                      if (window.pageYOffset > sticky) {
-                        header.classList.add("sticky");
-                        logo.classList.add("grab");
-                      } else {
-                        header.classList.remove("sticky");
-                        logo.classList.remove("grab");
-                      }
-                  }
-              }
-            }
   console.log('Main.js ready!')
 })
