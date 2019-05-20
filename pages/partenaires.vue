60ed3771cbb78492f431bf66b2ed76bf3284e8ab
Zymeth <Zymeth@users.noreply.github.com>
2019-05-20 14:58:41 +0200

    Update partenaires.vue

--- pages/partenaires.vue
+++ pages/partenaires.vue
@@ -1,5 +1,9 @@
 <template>
 <div id="parallax" data-image-src="https://electrobotik.netlify.com/images/crop.jpg">
+
+  <img id="logo" src="assets/images/logo.svg" alt="logo">
+  <img class="small-logo" src="assets/images/logo-horizontal.svg" alt="logo">
+
   <div class="container-grid scene_element scene_element--fadein">
           <div class="background"></div>
           <h2>PARTENAIRES</h2>
