0a4d5d4885fd313a962a8147213c329a10108c07
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-20 12:54:17 +0200

    Update festival.vue

--- pages/festival.vue
+++ pages/festival.vue
@@ -24,8 +24,3 @@
     }
   }
 </script>
-<style>
-  @media only screen and (min-width:960px) and (max-width:1264px){}
-  @media only screen and (min-width:600px) and (max-width:959px){}
-  @media only screen and (max-width:599px){}
-</style>
