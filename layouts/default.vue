037a706e0f35b49e79dfc6c579c7f9fa7a920e66
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-20 13:13:45 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="main" class="layout-electrobotik">
-  <transition-group name="page">
     <electrobotik-menu/>
     <nuxt/>
-  </transition-group>
   </div>
 </template>
 <script>
