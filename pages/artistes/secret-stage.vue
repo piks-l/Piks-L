6075a86bc8bb368be9fae97c42d491943c794130
rbiamsailor <50863502+rbiamsailor@users.noreply.github.com>
2019-05-21 16:22:56 +0200

    Update secret-stage.vue

--- pages/artistes/secret-stage.vue
+++ pages/artistes/secret-stage.vue
@@ -22,12 +22,19 @@
                     <p>VENDREDI ET SAMEDI</p>
                 </div>
                 <div class="content">
-                  <p>lol</p>
+                  <p><strong>Cette année, un scène sera caché à l’intérieur du festival ! </strong><br/><br/>
+                  Aucun indice ne sera dévoilé, seul les festivaliers les plus curieux pourront trouver la scène secrète 
+                  et auront la chance de profiter des sets du crew <strong>1RDT</strong> avec <strong>Charlotte (RA+RE )</strong>,
+                  <strong>D-Mood Records</strong>,
+               <strong>Def. Raw Music Concept</strong>, <strong>GetDub</strong>, <strong>IN' OUBLIABLES</strong> , <strong>Key Horses</strong>, 
+                  <strong>LES MULES</strong>, <strong>MendelBoy (Live)</strong>, <strong>Missing No</strong>, <strong>Thibaut Garenq</strong>.
+<br/><br/>
+<strong>Sortez vos boussoles, prenez la carte du festival et lancez-vous à sa recherche !!</strong></p>
                 </div>
             </div>
         </div>
         <div class="video">
-            <p>test</p>
+            <p><iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/117022940&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></p>
         </div>
       </div>
     </div>  
@@ -45,5 +52,5 @@ export default {
 };
 </script>
 <style>
-.decoration-CAMPING {background-image: url('https://electrobotik.netlify.com/images/scene-TECHNO.jpg');}
+.decoration-CAMPING {background-image: url('https://electrobotik.netlify.com/images/scene-SECRET.jpg');}
 </style>
