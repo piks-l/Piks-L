faa289ef23d3bcdf36dcb5e64a1709d3aa19db0d
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-21 15:26:51 +0200

    Create london-bus-stage.vue

---
+++ pages/artistes/london-bus-stage.vue
@@ -0,0 +1,49 @@
+<template> 
+  <div class="page artist">
+    <div class="decoration decoration-CAMPING">
+      <div class="volet-artist"></div>
+    </div>
+    <div id="main-container" class="artiste">
+      <div class="container scene_element scene_element--fadein ">
+        <nuxt-link to="/programmation/" class="fake-close">
+            <div class="left"></div>
+            <div class="right"></div>
+        </nuxt-link>
+        <div class="title">
+            <div class="background"></div>
+            <h2>LONDON BUS STAGE</h2>
+        </div>
+        <div class="content-artiste">
+            <div class="vertical-align">
+                <div class="artiste-stage">
+                    <p>LONDON BUS STAGE</p>
+                </div>
+                <div class="artiste-date">
+                    <p>VENDREDI ET SAMEDI</p>
+                </div>
+                <div class="content">
+                  <p>lol</p>
+                </div>
+            </div>
+        </div>
+        <div class="video">
+            <p>test</p>
+        </div>
+      </div>
+    </div>  
+  </div>
+</template>
+<script>
+import $ from 'jquery'
+import VueMarkdown from 'vue-markdown'
+import VueLazyload from 'vue-lazyload'
+export default {
+  layout: 'default',
+  components: { VueMarkdown, VueLazyload },
+  mounted() {
+  }
+};
+</script>
+<style>
+.decoration-CAMPING {background-image: url('https://electrobotik.netlify.com/images/scene-TECHNO.jpg');}
+</style>
