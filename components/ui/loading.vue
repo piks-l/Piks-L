f2b37f21716a2f1046b02b6cc1ebcdd764151557
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-22 04:41:01 +0200

    Update loading.vue

--- components/ui/loading.vue
+++ components/ui/loading.vue
@@ -1,12 +1,16 @@
 <template>
-  <div v-if="loading" class="loading-page">
-    <div class="big-logo">
-      <img class="logo large-logo" src="https://electrobotik.netlify.com/images/logo.svg"  alt="Logo Electrobotik Invasion">
-      <img class="logo small-logo" src="https://electrobotik.netlify.com/images/logo-horizontal.svg"  alt="Logo Electrobotik Invasion">
-    </div>
-  </div>
+<div v-if="loading" class="loading">
+<div class="eletrobotik-loading">
+ <div class="box">
+   <nuxt-link to="/"><img class="logo" src="https://electrobotik.netlify.com/images/logo-horizontal.svg" alt="Logo Electrobotik Invasion"></nuxt-link>
+   <h3>02&amp;03 AOÛT 2019 / CIRCUIT PAUL-RICARD</h3>
+ </div>
+</div>
+ <div class="circle-loading" >
+  <img src="https://electrobotik.netlify.com/images/circle.svg">
+ </div>
+ </div>
 </template>
-
 <script>
 export default {
   data: () => ({
@@ -24,15 +28,60 @@ export default {
 </script>
 
 <style>
-  .loading-page {
-    z-index:666;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 1);
-    text-align: center;
-    font-size: 30px;
-  }
+.eletrobotik-loading{
+  width:100%;
+  height:100vh;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
+eletrobotik-loading .logo {
+  position: relative;
+  max-width: auto;
+  max-height: auto;
+  top: 0px;
+  transform: none;
+}
+.eletrobotik-loading .box{
+  width:500px;
+  height:auto;
+  z-index:1!important;
+}
+.eletrobotik-loading h3 {
+  color:white; 
+  text-align:center;
+  border-top:5px solid white;
+  padding-top:25px;
+}
+.circle-loading {
+	position: fixed;
+	opacity: 1;
+	width: 350px;
+	height: 350px;
+	top: 50%;
+	left: 50%;
+	-webkit-transition: all .25s ease;
+	transition: all .25s ease;
+	-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+	-webkit-transform: translate(-50%, -50%);
+	transform: translate(-50%, -50%);
+}
+.circle-loading img {
+	width: 100%;
+	height: 100%;
+	-webkit-animation-name: circle;
+	animation-name: circle;
+	-webkit-animation-duration: 3s;
+	animation-duration: 3s;
+	-webkit-animation-iteration-count: infinite;
+	animation-iteration-count: infinite;
+}
+@-webkit-keyframes circle {
+  from {-webkit-transform:rotate(0deg);transform:rotate(0deg)}
+  to {-webkit-transform:rotate(360deg);transform:rotate(360deg)}
+}
+@keyframes circle {
+  from {-webkit-transform:rotate(0deg);transform:rotate(0deg)}
+  to {-webkit-transform:rotate(360deg);transform:rotate(360deg)}
+}
 </style>
