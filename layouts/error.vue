17754677f4effceea8eda4292b9e794e2ce6b082
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-23 14:28:59 +0200

    Update error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
-  </div>
+<div class="loading">
+<div class="eletrobotik-loading">
+ <div class="box">
+   <nuxt-link to="/"><img class="logo" src="https://electrobotik.netlify.com/images/logo-horizontal.svg" alt="Logo Electrobotik Invasion"></nuxt-link>
+    <h3>02&amp;03 AOÛT 2019 / CIRCUIT PAUL-RICARD</h3>
+    <nuxt-link to="/"><h1 v-if="error.statusCode === 404">Page non trouvée</h1></nuxt-link>
+    <h1 v-else>Une erreur est survenue !</h1>
+ </div>
+</div>
+ <div class="circle-loading" >
+  <img src="https://electrobotik.netlify.com/images/circle.svg">
+ </div>
+ </div>
 </template>
 
 <script>
@@ -12,3 +20,63 @@ export default {
   layout: 'default' // you can set a custom layout for the error page
 }
 </script>
+<style>
+.eletrobotik-loading{
+  width:100%;
+  height:100vh;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  background:black;
+  z-index:1;
+}
+.eletrobotik-loading .logo {
+  position: relative;
+  max-width: auto;
+  max-height: auto;
+  top: 0px;
+  transform: none;
+}
+.eletrobotik-loading .box{
+  width:500px;
+  height:auto;
+  z-index:2!important;
+}
+.eletrobotik-loading h3 {
+  color:white; 
+  text-align:center;
+  border-top:5px solid white;
+  padding-top:25px;
+}
+.circle-loading {
+	position: fixed;
+	opacity: 1;
+	width: 350px;
+	height: 350px;
+	top: 50%;
+	left: 50%;
+	-webkit-transition: all .25s ease;
+	transition: all .25s ease;
+	-ms-filter: "progid:DXImageTransform.Microsoft.Alpha(Opacity=0)";
+	-webkit-transform: translate(-50%, -50%);
+	transform: translate(-50%, -50%);
+}
+.circle-loading img {
+	width: 100%;
+	height: 100%;
+	-webkit-animation-name: circle;
+	animation-name: circle;
+	-webkit-animation-duration: 3s;
+	animation-duration: 3s;
+	-webkit-animation-iteration-count: infinite;
+	animation-iteration-count: infinite;
+}
+@-webkit-keyframes circle {
+  from {-webkit-transform:rotate(0deg);transform:rotate(0deg)}
+  to {-webkit-transform:rotate(360deg);transform:rotate(360deg)}
+}
+@keyframes circle {
+  from {-webkit-transform:rotate(0deg);transform:rotate(0deg)}
+  to {-webkit-transform:rotate(360deg);transform:rotate(360deg)}
+}
+</style>
