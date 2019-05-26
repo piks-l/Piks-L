75cdfe15ef89662eaf0246e32c16a2dcd09b02a3
Puxian AlHazred <yoann@piks-l.com>
2019-05-26 23:57:48 +0200

    Update _slug.vue

--- pages/lieux/_slug.vue
+++ pages/lieux/_slug.vue
@@ -1,27 +1,11 @@
 <template>
   <div class="page artist">
-    <div :class="'decoration decoration-'+stage">
-      <div class="volet-artist"></div>
-    </div>
     <div id="main-container" class="artiste">
       <div class="container scene_element scene_element--fadein ">
-        <nuxt-link to="/programmation/" class="fake-close">
-            <div class="left"></div>
-            <div class="right"></div>
-        </nuxt-link>
         <div class="title">
-            <div class="background"></div>
             <h2>{{ title }}</h2>
-        </div>
-        <div class="content-artiste">
-            <div class="vertical-align">
-                <div class="content">
-                  <vue-markdown>{{description}}</vue-markdown>
-                </div>
-                <div class="social-artiste">
-                  <a target="_blank" v-for="socia in social" :key="socia.awesome" :href="socia.lien"><i :class="socia.awesome"></i></a>
-                </div>
-            </div>
+            <vue-markdown>{{description}}</vue-markdown>
+            <p target="_blank" v-for="Evenements in Evenement" :key="Evenements.events">{{Evenements.event}}</p>
         </div>
         <div class="video">
             <vue-markdown>{{iframe}}</vue-markdown>
@@ -38,8 +22,8 @@ export default {
   layout: 'default',
   components: { VueMarkdown, VueLazyload },
   async asyncData({ params }) {
-    let page = await import('~/content/artistes/page/' + params.slug + '.json');
-    return page;
+    let pageLieux = await import('~/content/lieux/page/' + params.slug + '.json');
+    return pageLieux;
   },
   head() {
     return {
