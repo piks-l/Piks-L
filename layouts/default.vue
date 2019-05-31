c9a3de1a31ee68e0cb1cd26956b55e737a2962d9
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-31 13:30:50 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@
   import eaBackground from '~/components/ui/background.vue'
   export default {
       transition: {
-        name: 'page',
+        name: 'intro',
         mode: 'out-in'
       },
       components: {
@@ -25,3 +25,60 @@
       }
   }
 </script>
+<style>
+.intro-enter-active,
+.intro-leave-active {
+  transition-duration: 1600ms;
+}
+.intro-enter-active::before, .intro-enter-active::after,
+.intro-leave-active::before,
+.intro-leave-active::after {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  display: block;
+  width: 100%;
+  height: 50%;
+  transition-property: opacity, transform;
+  transition-timing-function: ease-in-out;
+}
+.intro-enter-active::before,
+.intro-leave-active::before {
+  background-color: #2e2e2e;
+}
+.intro-enter-active::after,
+.intro-leave-active::after {
+  top: 50%;
+  background-color: #2e2e2e;
+}
+.intro-leave::before, .intro-leave::after {
+  transform: scaleX(0);
+}
+.intro-leave-active::before {
+  transition-duration: 800ms;
+}
+.intro-leave-active::after {
+  transition-duration: 500ms;
+  transition-delay: 300ms;
+}
+.intro-leave-to::before, .intro-leave-to::after {
+  transform: scale(1);
+  transform-origin: left;
+}
+.intro-enter::before, .intro-enter::after {
+  transform: scaleX(1);
+}
+.intro-enter-active::before {
+  transition-duration: 800ms;
+}
+.intro-enter-active::after {
+  transition-duration: 500ms;
+  transition-delay: 300ms;
+}
+.intro-enter-to::before, .intro-enter-to::after {
+  transform: scaleX(0);
+  transform-origin: right;
+}
+</style>
