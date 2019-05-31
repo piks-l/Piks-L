e3ba17456837eb74c504d5bb0cf995fe8124f138
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-05-31 11:54:54 +0200

    Create simple.vue

---
+++ layouts/simple.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main" class="layout-ea">
+    <ea-background/>
+    <ea-menu/>
+    <nuxt/>
+  </div>
+</template>
+<script>
+  import $ from 'jquery'
+  import eaMenu from '~/components/ui/menu.vue'
+  import eaBackground from '~/components/ui/background.vue'
+  export default {
+      transition: 'page',
+      components: {
+          eaMenu,
+          eaBackground
+      },
+      mounted () {
+      }
+  }
+</script>
