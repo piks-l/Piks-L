0f8c33f0439e23413953b6a6389032b9ba5b40cf
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 13:27:47 +0200

    Update background.vue

--- components/ui/background.vue
+++ components/ui/background.vue
@@ -1,10 +1,10 @@
 <template>
 <header class="fond-principal">
-  <img class="seconde-background" src="https://piks-l.com/codepen/background-1-electro-alternativ.jpg">
+  <img class="seconde-background" src="/images/background-1-electro-alternativ.jpg">
 	<div class="mirror anim-filter" >
 		<div class="mirror__source mirror__source--solid ">
 			<svg id="main-background" version="1.1"  xmlns="http://www.w3.org/2000/svg" style="enable-background:new 0 0 1920 1080" viewBox="0 0 1920 1080" x="0px" xml:space="preserve" xmlns:xlink="http://www.w3.org/1999/xlink" y="0px" preserveAspectRatio="xMidYMid meet">
-				<image class="main-background" height="100%" style="overflow:visible" width="100%" xlink:href="https://piks-l.com/codepen/background-1-electro-alternativ.jpg"></image>			
+				<image class="main-background" height="100%" style="overflow:visible" width="100%" xlink:href="/images/background-1-electro-alternativ.jpg"></image>			
 			</svg>
 		</div>
 	</div>
