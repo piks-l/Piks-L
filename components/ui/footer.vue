a72063133816deb801dfc2d89a45acb49fbdbcb6
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 14:18:49 +0200

    Update footer.vue

--- components/ui/footer.vue
+++ components/ui/footer.vue
@@ -1,13 +1,14 @@
 <template>
   <footer id="footer">
     <div class="reseaux">
-      <a target="_blank" href="https://www.facebook.com/ElectrobotikInvasion"><i class="fab fa-facebook-f"></i></a>
-      <a target="_blank" href="https://www.instagram.com/electrobotik_invasion/"><i class="fab fa-instagram"></i></a>
-      <a target="_blank" href="https://www.youtube.com/channel/UCin2oNXJUVgk7jYliy-dg5A"><i class="fab fa-youtube"></i></a>
-      <nuxt-link to="/billetterie/"><i class="fas fa-ticket-alt"></i></nuxt-link>
+      <a target="_blank" href=""><i class="fab fa-facebook-f"></i></a>
+      <a target="_blank" href=""><i class="fab fa-instagram"></i></a>
+      <a target="_blank" href=""><i class="fab fa-youtube"></i></a>
+      <a target="_blank" href=""><i class="fab fa-twitter"></i></a>
+      <a target="_blank" href=""><i class="fab fa-spotify"></i></a>
     </div>
     <div class="copyright">
-      <p>© www.electrobotikinvasion.com - Tous droits réservés</p>
+      <p>© www.electro-alternativ.com - 15E - Tous droits réservés</p>
     </div>
   </footer>
 </template>
@@ -18,66 +19,3 @@ export default {
   }
 }
 </script>
-<style>
-/* FOOTER */
-#footer {
-  overflow: hidden;
-  /*position: relative;*/
-  bottom: 0;
-  width: 100%;
-  padding:0px;
-}
-
-/* FOOTER GENERAL */
-#footer .reseaux,
-#footer .copyright {
-  display: block;
-  position: relative;
-  text-align: center;
-}
-#footer .reseaux {
-    background: linear-gradient(0.25turn, #0079ff, #b24576);
-}
-
-#footer .copyright {
-  background: #000;
-}
-#footer .copyright a {
-  color: #FFFFFF;
-}
-#footer .copyright a:hover {
-  color: #FCFCFC;
-}
-
-#footer a {
-  color: #FFFFFF;
-  padding: 15px;
-  text-decoration: none;
-  display: inline-block;
-  position: relative;
-}
-#footer a:hover {
-  color: #cecece;
-}
-
-/* FOOTER NAVIGATION */
-
-
-/* FOOTER RESEAUX */
-#footer .reseaux a {
- color: #FFF;
- font-size: 23px;
-}
-#footer .reseaux a:hover {
-  color: #000000;
-}
-
-/* FOOTER COPYRIGHT */
-#footer .copyright p {
-    line-height: 51px;
-}
-#footer .copyright a {
-    padding: 0;
-}
-
-</style>
