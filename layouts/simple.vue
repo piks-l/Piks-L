ca756dec997360a489b87cd37f3e560781586959
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 20:56:52 +0200

    Update simple.vue

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -1,16 +1,18 @@
 <template>
+<transition name="intro" mode="out-in">
   <div id="main" class="layout-ea">
     <ea-logo/>
     <ea-menu/>
+    
     <nuxt/>
   </div>
+</transition>
 </template>
 <script>
   import $ from 'jquery'
   import eaLogo from '~/components/ui/logo.vue'
   import eaMenu from '~/components/ui/menu.vue'
   export default {
-      transition: { name: 'intro', mode: 'out-in' },
       components: {
           eaLogo,
           eaMenu
