39aa867a45e2318b935162bce838c1a42186eec2
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 14:22:23 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <template>
   <main>
-    <ea-background/>
     <h1>ACCUEIL</h1>
     <nuxt-link to="/programmations/">TEST</nuxt-link>
   </main>
 </template>
-
 <script>
   import $ from 'jquery'
-  import eaBackground from '~/components/ui/background.vue'
   // export
   export default {
     layout: 'default',
-    components: { eaBackground },
     data() {
       return {
         changed: false
@@ -20,5 +16,3 @@
     }
   }
 </script>
-<style>
-</style>
