fba6ecfd43a72b9d6756f9eeb3b9134b420704ba
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 20:57:28 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
+<transition name="intro" mode="out-in">
   <div id="main" class="layout-ea">
     <ea-background/>
     <ea-logo/>
     <ea-menu/>
     <nuxt/>
   </div>
+  </transition>
 </template>
 <script>
   import $ from 'jquery'
@@ -12,9 +14,6 @@
   import eaMenu from '~/components/ui/menu.vue'
   import eaBackground from '~/components/ui/background.vue'
   export default {
-      transition: {
-        name: 'intro'
-      },
       components: {
           eaLogo,
           eaMenu,
