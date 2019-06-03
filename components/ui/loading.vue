736548b16fc160ce9f063840262d71daa06109c2
Zymeth <Zymeth@users.noreply.github.com>
2019-06-03 18:41:33 +0200

    Update loading.vue

--- components/ui/loading.vue
+++ components/ui/loading.vue
@@ -1,11 +1,13 @@
 <template>
   <div v-if="loading" class="loading">
+    <div class="center">
       <ea-logo/>
       <div class="deco-video">
         <video autoplay="autoplay" loop="loop" muted="muted">
           <source src="/videos/etiquette-noir.mp4" type="video/mp4">
         </video>
       </div>
+    </div>
   </div>
 </template>
 <script>
