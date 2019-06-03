24d912bbec4db41695adcb83efbdc09166785811
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 15:00:04 +0200

    Update index.vue

--- pages/soirees/index.vue
+++ pages/soirees/index.vue
@@ -1,43 +1,35 @@
 <template>
   <div id="main-container">
-    <div id="parallax" data-image-src="https://electrobotik.netlify.com/images/crop.jpg">
       <div  class="container scene_element scene_element--fadein padding">
-        <img id="logo" src="https://electrobotik.netlify.com/images/logo-basic.svg" alt="logo">
         <header id="header">
           <div class="sticky-container">
-              <img id="sticky-logo" class="logo" src="https://electrobotik.netlify.com/images/logo-horizontal.svg"  alt="logo">
               <div id="filters">
-                <h1>PROGRAMMATION</h1>
+                <h1>SOIREES</h1>
                 <div class="button-date button-group"  data-filter-group="date">
                     <div class="center">
-                        <button class="button is-checked" @click="filter('*')" data-filter="*">FULL LINE-UP</button>
+                        <button class="button is-checked" @click="filter('*')" data-filter="*">ALL</button>
                         <span>/</span>
-                        <button class="button" @click="filter('.vendredi')" data-filter=".vendredi">VEN. 02 AOÛT</button>
+                        <button class="button" @click="filter('.vendredi')" data-filter=".vendredi">Jours</button>
                         <span>/</span>
-                        <button class="button" @click="filter('.samedi')" data-filter=".samedi">SAM. 03 AOÛT</button>
+                        <button class="button" @click="filter('.samedi')" data-filter=".samedi">Soirées</button>
+                        <span>/</span>
+                        <button class="button" @click="filter('.samedi')" data-filter=".samedi">Nuits</button>
                     </div>
                 </div>
                 <div class="stage">
                   <div class="center">
                     <div class="button-stage button-group"  data-filter-group="stage">
-                        <button class="button active" @click="filter('.HARDCORE')" data-filter=".HARDCORE"><span>T-800 -</span> HARDCORE STAGE</button>
-                        <button class="button active" @click="filter('.TECHNO')" data-filter=".TECHNO"><span>R2-D2 -</span> TECHNO STAGE</button>
-                        <button class="button active" @click="filter('.TRANCE')" data-filter=".TRANCE"><span>WALL-E -</span> TRANCE STAGE</button>
-                        <button class="button active" @click="filter('.HIPHOP')" data-filter=".HIPHOP"><span>C-3PO -</span> HIP-HOP STAGE</button>
+                        <button class="button active" @click="filter('.HARDCORE')" data-filter=".HARDCORE"><span>T-800 -</span> SEMAINE 1</button>
+                        <button class="button active" @click="filter('.TECHNO')" data-filter=".TECHNO"><span>R2-D2 -</span> SEMAINE 2</button>
+                        <button class="button active" @click="filter('.TRANCE')" data-filter=".TRANCE"><span>WALL-E -</span> SEMAINE 3</button>
+                        <button class="button active" @click="filter('.HIPHOP')" data-filter=".HIPHOP"><span>C-3PO -</span> SEMAINE 4</button>
                         <button class="button any" data-filter=""></button>
-                    </div>
-                    <div class="other-stage">
-                        <button class="buton active"><nuxt-link to="/artistes/secret-stage/">SECRET STAGE</nuxt-link></button>
-                        <button class="buton active"><nuxt-link to="/artistes/london-bus-stage/">LONDON BUS STAGE</nuxt-link></button>
-                        <button class="buton active"><nuxt-link to="/artistes/camping-stage/">CAMPING STAGE</nuxt-link></button>
                     </div>
                   </div>
                 </div>
               </div>
-              <div class="separator"></div>
           </div>
       </header>
-      <div class="deco-top"><div id="parallax1" class="parallax" data-image-src="https://electrobotik.netlify.com/images/crop.jpg"></div></div>
           <div class="grid">
               <div v-for="artiste in artistes" :key="artiste.date"  :class="artiste.dateshow+' '+artiste.stage" class="element-item">
                    <nuxt-link :to="artiste._path+'/'">
@@ -45,7 +37,6 @@
                           <div class="volet1">
                               <img class="lazy" v-lazy="artiste.thumbnail" :alt="artiste.title">
                               <div class="boxshadow"></div>
-                              <div class="circle"><img src="https://electrobotik.netlify.com/images/circle.svg"/></div>
                               <h3 class="name small">{{artiste.title}}</h3>
                           </div>
                           <div class="volet2"></div>
@@ -55,8 +46,6 @@
               </div>
           </div>
       </div>
-      <electrobotik-footer/>
-     </div>
   </div>
 </template>
 <script>
