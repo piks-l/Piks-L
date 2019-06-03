f3fb72acee60f95398328ee444cd993e479dc267
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 16:59:15 +0200

    Update _slug.vue

--- pages/programmation/_slug.vue
+++ pages/programmation/_slug.vue
@@ -1,30 +1,10 @@
 <template>
   <div class="page artist">
-    <div :class="'decoration decoration-'+stage">
-      <div class="volet-artist"></div>
-    </div>
-    <div id="main-container" class="artiste">
-      <div class="container scene_element scene_element--fadein ">
-        <nuxt-link to="/programmation/" class="fake-close">
-            <div class="left"></div>
-            <div class="right"></div>
-        </nuxt-link>
-        <div class="title">
-            <div class="background"></div>
-            <h2>{{ title }}</h2>
-        </div>
-        <div class="content-artiste">
-            <div class="vertical-align">
-                <div class="content">
-                  <vue-markdown>{{description}}</vue-markdown>
-                </div>
-                <div class="social-artiste">
-                  <a target="_blank" v-for="socia in social" :key="socia.awesome" :href="socia.lien"><i :class="socia.awesome"></i></a>
-                </div>
-            </div>
-        </div>
-      </div>
-    </div>
+     <h2>{{ title }}</h2>
+     <p>{{ semaine }}</p>
+     <p>{{ type }}</p>
+     <p>{{ start }} > {{ end }}</p>
+     
   </div>
 </template>
 <script>
