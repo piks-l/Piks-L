1869cfadf7a0e986a6abe73505a583b1d8f1cacf
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-03 19:31:24 +0200

    Update _slug.vue

--- pages/programmation/_slug.vue
+++ pages/programmation/_slug.vue
@@ -4,8 +4,28 @@
      <p>{{ semaine }}</p>
      <p>{{ type }}</p>
      <p>{{ start }} > {{ end }}</p>    
+      <header class="fond-principal">
+        <img class="seconde-background" :src="background">
+        <div class="mirror anim-filter" >
+          <div class="mirror__source mirror__source--solid ">
+            <svg id="main-background" version="1.1"  xmlns="http://www.w3.org/2000/svg" style="enable-background:new 0 0 1920 1080" viewBox="0 0 1920 1080" x="0px" xml:space="preserve" xmlns:xlink="http://www.w3.org/1999/xlink" y="0px" preserveAspectRatio="xMidYMid meet">
+              <image class="main-background" height="100%" style="overflow:visible" width="100%" :xlink:href="background"></image>			
+            </svg>
+          </div>
+        </div>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="glitch">
+              <feTurbulence baseFrequency="0.03 0.01" id="turb" numOctaves="1" result="warp" type="fractalNoise"></feTurbulence>
+              <feColorMatrix in="warp" result="huedturb" type="hueRotate" values="90">
+                <animate attributeName="values" attributeType="XML" dur="10s" repeatCount="indefinite" values="0;180;360"></animate>
+              </feColorMatrix>
+              <feDisplacementMap in="SourceGraphic" in2="huedturb" scale="30" xChannelSelector="R" yChannelSelector="G"></feDisplacementMap>
+            </filter>
+          </defs>
+        </svg>
+      </header>
      <img style="display:none" id="background-image-event" :src="background" />
-     
   </main>
 </template>
 <script>
@@ -13,12 +33,12 @@ import $ from 'jquery'
 import VueMarkdown from 'vue-markdown'
 import VueLazyload from 'vue-lazyload'
 export default {
-  layout: 'default',
+  layout: 'simple',
   transition: { name: 'intro', mode: 'out-in' },
   components: { VueMarkdown, VueLazyload },
   async asyncData({ params }) {
     const { data } = await import('~/content/programmation/page/' + params.slug + '.json')
-    return { page: data, page2: data.background }
+    return { page: data }
   },
   head() {
     return {
@@ -32,9 +52,6 @@ export default {
     }
   },
   mounted() {
-    var back =  $('#background-image-event').attr('src');
-    $('.main-background').attr('href', back);
-    $('#background-image-event').remove();
   }
 };
 </script>
