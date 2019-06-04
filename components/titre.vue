5dda44071e9e2230671af07525672bae9ebc8440
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-04 15:16:47 +0200

    Create titre.vue

---
+++ components/titre.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2 class="ea-titre">{{this.$route.name}}</h2>
+</template>
+<script>
+import $ from 'jquery'
+export default {
+  mounted() {
+    this.title();
+  },
+  methods: {
+    title() {
+     console.log("Titre de la page loaded")
+    }
+  }
+}
+</script>
