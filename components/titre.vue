ecb8c385e791be38f64deb26c06b575a63064452
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-04 15:30:46 +0200

    Update titre.vue

--- components/titre.vue
+++ components/titre.vue
@@ -14,3 +14,20 @@ export default {
   }
 }
 </script>
+<style>
+.ea-titre {
+  line-height: 60px;
+  position: relative;
+  font-size: 4em;
+  text-transform: uppercase;
+  color: white;
+  text-shadow: 0px 0px 30px rgba(0,0,0,.5);
+  animation: turn 2s linear forwards 1s;
+  transform: rotateX(180deg);
+}
+@keyframes turn {
+  100% {
+    transform: rotateX(0deg);
+  }
+}
+</style>
