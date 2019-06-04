5fb40f465650c398b27bcc9beca102cd039603d0
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-04 15:43:47 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="main" class="ea-layout">
     <header class="ea-ui">
+      <ui-loading/>
       <ui-background/>
       <ui-logo/>
       <ui-menu class="ea-menu" />
@@ -11,12 +12,14 @@
 </template>
 <script>
   import $ from 'jquery'
+  import uiLoading from '~/components/ui/loading.vue'
   import uiLogo from '~/components/ui/logo.vue'
   import uiMenu from '~/components/ui/menu.vue'
   import uiBackground from '~/components/ui/background.vue'
   import eaTitre from '~/components/titre.vue'
   export default {
       components: {
+          uiLoading,
           uiLogo,
           uiMenu,
           uiBackground,
