7ced49bd32b0f859187c679366ce5312337a9d58
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-06 12:37:02 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -25,12 +25,5 @@
     .page-enter, .page-leave-active {
       opacity: 0;
       transform-origin: 50% 50%;
-    }
-    .page-enter-active .ea-titre, .page-leave-active .ea-titre{
-      transition: all .50s ease-out;
-    }
-    .page-enter .ea-titre, .page-leave-active .ea-titre{
-        opacity: 0;
-        margin-bottom: 50px;
-      }    
+    }   
 </style>
