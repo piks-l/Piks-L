5259a8c08fadfd3b9047a777c603b55d8e09d385
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-07 12:40:39 +0200

    Update simple.vue

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -1,18 +1,31 @@
 <template>
-  <div id="main" class="layout-ea">
-    <ea-logo/>
-    <ea-menu/>
-    <nuxt class="main-evenements"/>
+  <div id="main" class="ea-layout">
+    <header class="ea-ui">
+      <ui-loading/>
+      <ui-background/>
+      <ui-logo/>
+      <div class="deco-video">
+          <video autoplay="autoplay" loop="loop" muted="muted">
+            <source src="/videos/etiquette-noir.mp4" type="video/mp4">
+          </video>
+      </div>
+      <ui-menu class="ea-menu" />
+    </header>
+    <nuxt/>
   </div>
 </template>
 <script>
   import $ from 'jquery'
-  import eaLogo from '~/components/ui/logo.vue'
-  import eaMenu from '~/components/ui/menu.vue'
+  import uiLoading from '~/components/ui/loading.vue'
+  import uiLogo from '~/components/ui/logo.vue'
+  import uiMenu from '~/components/ui/menu.vue'
+  import uiBackground from '~/components/ui/background.vue'
   export default {
       components: {
-          eaLogo,
-          eaMenu
+          uiLoading,
+          uiLogo,
+          uiMenu,
+          uiBackground
       },
       mounted () {
       }
