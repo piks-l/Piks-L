83528ed34d3ba4498dabf220242b9d6185ff194d
Zymeth <Zymeth@users.noreply.github.com>
2019-06-07 15:24:49 +0200

    Update index.vue

--- pages/programmation/index.vue
+++ pages/programmation/index.vue
@@ -102,7 +102,7 @@ export default {
             });
             $('.button-group').each( function( i, buttonGroup ) {
                 var $buttonGroup = $( buttonGroup );
-                $buttonGroup.on( 'click', 'button', function( event ) {
+                $buttonGroup.on( 'click', '.button', function( event ) {
                   $buttonGroup.find('.is-checked').removeClass('is-checked');
                   var $button = $( event.currentTarget );
                   $button.addClass('is-checked');
