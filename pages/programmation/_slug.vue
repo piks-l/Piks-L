ff69419a7507916492b98401657c6bf09d37677f
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-07 12:34:33 +0200

    Update _slug.vue

--- pages/programmation/_slug.vue
+++ pages/programmation/_slug.vue
@@ -1,31 +1,33 @@
 <template>
-  <main class="page artist">
-        <header class="fond-principal">
-        <img class="seconde-background" :src="background">
-        <div class="mirror anim-filter" >
-          <div class="mirror__source mirror__source--solid ">
-            <svg id="main-background" version="1.1"  xmlns="http://www.w3.org/2000/svg" style="enable-background:new 0 0 1920 1080" viewBox="0 0 1920 1080" x="0px" xml:space="preserve" xmlns:xlink="http://www.w3.org/1999/xlink" y="0px" preserveAspectRatio="xMidYMid meet">
-              <image class="main-background" height="100%" style="overflow:visible" width="100%" :xlink:href="background"></image>			
-            </svg>
-          </div>
-        </div>
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            <filter id="glitch">
-              <feTurbulence baseFrequency="0.03 0.01" id="turb" numOctaves="1" result="warp" type="fractalNoise"></feTurbulence>
-              <feColorMatrix in="warp" result="huedturb" type="hueRotate" values="90">
-                <animate attributeName="values" attributeType="XML" dur="10s" repeatCount="indefinite" values="0;180;360"></animate>
-              </feColorMatrix>
-              <feDisplacementMap in="SourceGraphic" in2="huedturb" scale="30" xChannelSelector="R" yChannelSelector="G"></feDisplacementMap>
-            </filter>
-          </defs>
+<div>
+   <header class="fond-principal">
+    <img class="seconde-background" :src="background">
+    <div class="mirror anim-filter" >
+      <div class="mirror__source mirror__source--solid ">
+        <svg id="main-background" version="1.1"  xmlns="http://www.w3.org/2000/svg" style="enable-background:new 0 0 1920 1080" viewBox="0 0 1920 1080" x="0px" xml:space="preserve" xmlns:xlink="http://www.w3.org/1999/xlink" y="0px" preserveAspectRatio="xMidYMid meet">
+          <image class="main-background" height="100%" style="overflow:visible" width="100%" :xlink:href="background"></image>			
         </svg>
-      </header>
+      </div>
+    </div>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <defs>
+        <filter id="glitch">
+          <feTurbulence baseFrequency="0.03 0.01" id="turb" numOctaves="1" result="warp" type="fractalNoise"></feTurbulence>
+          <feColorMatrix in="warp" result="huedturb" type="hueRotate" values="90">
+            <animate attributeName="values" attributeType="XML" dur="10s" repeatCount="indefinite" values="0;180;360"></animate>
+          </feColorMatrix>
+          <feDisplacementMap in="SourceGraphic" in2="huedturb" scale="30" xChannelSelector="R" yChannelSelector="G"></feDisplacementMap>
+        </filter>
+      </defs>
+    </svg>
+  </header>
+  <main class="page artist">
      <h2>{{ title }}</h2>
      <p>{{ semaine }}</p>
      <p>{{ type }}</p>
      <p>{{ start }} > {{ end }}</p>    
   </main>
+</div>
 </template>
 <script>
 import $ from 'jquery'
