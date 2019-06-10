d4c7e3fe21484f4686d6f1c81f5b0c2e39526e63
Zymeth <Zymeth@users.noreply.github.com>
2019-06-10 15:48:02 +0200

    Update sidebar-evenement.vue

--- components/sidebar-evenement.vue
+++ components/sidebar-evenement.vue
@@ -2,6 +2,7 @@
   <aside class="ea-sidebar-evenement">
     <p class="date-event">{{ datehumaine }}</p>
     <p class="date-time">{{ start }} > {{ end }}</p>
+    <div class=""><i class="fas fa-calendar-day"></i> <a href="#" alt="">PARTICIPER</a></div>
     <nuxt-link class="link-map" :to="'/lieux/'+lieu"><i class="fas fa-map-marked-alt"></i> {{ lieu }}</nuxt-link>
   </aside>
 </template>
