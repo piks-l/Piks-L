b3eed52c58a1ef55e6db2681a5ac3b1ba2b3289c
Zymeth <Zymeth@users.noreply.github.com>
2019-06-10 16:18:52 +0200

    Update sidebar-evenement.vue

--- components/sidebar-evenement.vue
+++ components/sidebar-evenement.vue
@@ -1,13 +1,15 @@
 <template>
-  <aside class="ea-sidebar-evenement">
-    <p class="date-event">{{ datehumaine }}</p>
-    <p class="date-time">{{ start }} > {{ end }}</p>
-    <div class="reserver"><i class="fas fa-calendar"></i> <a href="#" alt="">PARTICIPER</a></div>
-    <hr/>
-    <nuxt-link class="link-map" :to="'/lieux/'+lieu"><i class="fas fa-map-marked-alt"></i> {{ lieu }}</nuxt-link>
-    <hr/>
-    <nuxt-link class="retour" to="/programmation/"><b>PROGRAMMATION</b>RETOUR</nuxt-link>
-  </aside>
+  <div class="right-side">
+      <aside class="ea-sidebar-evenement">
+          <p class="date-event">{{ datehumaine }}</p>
+          <p class="date-time">{{ start }} > {{ end }}</p>
+          <div class="reserver"><i class="fas fa-calendar"></i> <a href="#" alt="">PARTICIPER</a></div>
+          <hr/>
+          <nuxt-link class="link-map" :to="'/lieux/'+lieu"><i class="fas fa-map-marked-alt"></i> {{ lieu }}</nuxt-link>
+          <hr/>        
+      </aside>
+      <nuxt-link class="retour" to="/programmation/"><b>PROGRAMMATION</b>RETOUR</nuxt-link>
+  </div>
 </template>
 <script>
 export default {
