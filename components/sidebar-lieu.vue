f0345fa7fb4bacea239b270c8be8c81e9551ea75
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-12 00:58:58 +0200

    Update sidebar-lieu.vue

--- components/sidebar-lieu.vue
+++ components/sidebar-lieu.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="right-side">
       <aside class="ea-sidebar-evenement">
-          <div class="link-map"><i class="fas fa-map-marked-alt"></i><a target="_blank" :href="gmap">{{ adresse }}</a></div>
+          <div class=""><i class="fas fa-map-marked-alt"></i><p><a target="_blank" :href="gmap">{{ adresse }}</a></p></div>
           <hr/>
-          <div class="link-map"><i class="fas fa-walking"></i><p>{{ direction }}</p></div>
+          <div class=""><i class="fas fa-walking"></i><p>{{ direction }}</p></div>
           <hr/>
           <nuxt-link class="retour" to="/lieux/"><b>LIEUX</b>RETOUR</nuxt-link>
 
