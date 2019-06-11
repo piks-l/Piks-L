274072e976dd8acde668fcada36fc816b7c6c2b4
Zymeth <Zymeth@users.noreply.github.com>
2019-06-11 19:14:16 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <header class="ea-ui">
       <ui-loading/>
       <ui-background/>
-      <ui-logo/>
+      <nuxt-link class="menu-link" to="/"><ea-logo/></nuxt-link>
       <div class="deco-video">
           <video autoplay="autoplay" loop="loop" muted="muted">
             <source src="/videos/etiquette-noir.mp4" type="video/mp4">
