aa28657c1f6eb32c1a8d31877981065aa59ff6fe
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-17 14:12:35 +0200

    Create social.vue

---
+++ components/social.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="right-side">
+    <aside class="ea-sidebar">
+      <div class="social">
+          <a target="_blank" href="https://www.facebook.com/electroalternativ" alt="Facebook EA"><i class="fab fa-facebook-f"></i></a>
+          <a target="_blank" href="https://twitter.com/electroalternat" alt="Twitter EA"><i class="fab fa-twitter"></i></a>
+          <a target="_blank" href="https://www.youtube.com/user/electroalternativ" alt="Youtube EA"><i class="fab fa-youtube"></i></a>
+          <a target="_blank" href="https://www.instagram.com/electroalternativ/" alt="Instagram EA"><i class="fab fa-instagram"></i></a>
+      </div>
+    </aside>
+  </div>
+</template>
