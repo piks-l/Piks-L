df0fa34d1ba47236ef78f61eee0bc9c5c92df884
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-18 15:14:47 +0200

    Create festival.vue

---
+++ pages/festival.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="black">
+    <ea-titre/>
+  </main>
+</template>
+<script>
+  import $ from 'jquery'
+  import eaTitre from '~/components/titre.vue'
+  export default {
+    layout: 'simple',
+    transition: { name: 'outro', mode: 'out-in' },
+    components: { eaTitre },
+    data() {
+      return {
+        changed: false
+      }
+    }
+  }
+</script>
