84c894c183cdfc9aeaee044bcd66e9b554574651
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-18 11:36:03 +0200

    Update simple.vue

--- layouts/simple.vue
+++ layouts/simple.vue
@@ -1,17 +1,26 @@
 <template>
-  <div id="main" class="ea-layout">
-    <header class="ea-ui">
-      <ui-loading/>
-      <ui-background/>
+  <div id="main" :class="'ea-layout-'+this.$route.name">
+    <div class="ea_col mobile-menu">
+      MENU
+    </div>
+    <div class="ea_col ui-left">
       <nuxt-link to="/"><ui-logo/></nuxt-link>
-      <div class="deco-video">
-          <video autoplay="autoplay" loop="loop" muted="muted">
-            <source src="/videos/etiquette-noir.mp4" type="video/mp4">
-          </video>
-      </div>
       <ui-menu class="ea-menu" />
-    </header>
-    <nuxt/>
+    </div>
+    <div class="ea_col ui-content">
+      <nuxt/>
+    </div>
+    <div class="ea_col ui-right">
+      <div class="deco-video">
+        <video autoplay="autoplay" loop="loop" muted="muted">
+          <source src="/videos/etiquette-noir.mp4" type="video/mp4">
+        </video>
+      </div>
+    </div>
+    <div class="ea_col ui-footer">
+      <ui-footer/>
+    </div>
+    <ui-background/>
   </div>
 </template>
 <script>
@@ -19,12 +28,14 @@
   import uiLoading from '~/components/ui/loading.vue'
   import uiLogo from '~/components/ui/logo.vue'
   import uiMenu from '~/components/ui/menu.vue'
+  import uiFooter from '~/components/ui/footer.vue'
   import uiBackground from '~/components/ui/background.vue'
   export default {
       components: {
           uiLoading,
           uiLogo,
           uiMenu,
+          uiFooter,
           uiBackground
       },
       mounted () {
