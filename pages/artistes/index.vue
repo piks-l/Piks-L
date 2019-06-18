88e72ebe35c6cdb0a2a4c0769a511dab19fc7cbb
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-18 10:36:55 +0200

    Update index.vue

--- pages/artistes/index.vue
+++ pages/artistes/index.vue
@@ -3,13 +3,12 @@
       <div class="container">
           <ea-titre/>
           <div class="grid">
-              <div class="stamp stamp1"></div>
-              <div class="stamp stamp2"></div>
               <div v-for="artiste in artistes" :key="artiste.url" class="element-item">
                   <nuxt-link :to="artiste._path+'/'" >
-                     <div class="ea-couverture-lieu" :style="{ backgroundImage: `url(${artiste.thumbnail})` }"></div>
-                     <p class="grid-artistes"><strong>{{artiste.title}}</strong></p>
-                     <p class="grid-intro">{{artiste.label}} <small>{{artiste.langue}}</small></p>
+                     <div class="ea-couverture-lieu" :style="{ backgroundImage: `url(${artiste.thumbnail})` }">
+                       <p class="grid-artistes"><strong>{{artiste.title}}</strong></p>
+                       <p class="grid-intro">{{artiste.label}} <small>{{artiste.langue}}</small></p>
+                     </div>
                   </nuxt-link>
               </div>
           </div>
