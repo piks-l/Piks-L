5979490fe7fd2ab0043e157e039a4ec0452d176c
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-21 13:33:21 +0200

    Create default-mono.vue

---
+++ layouts/default-mono.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="main" :class="'ea-layout-'+this.$route.name">
+    <header class="ea-ui">
+      <ui-loading/>
+      <nuxt-link to="/"><ui-logo/></nuxt-link>
+      <div class="deco-video">
+          <video autoplay="autoplay" loop="loop" muted="muted">
+            <source src="/videos/etiquette-noir.mp4" type="video/mp4">
+          </video>
+      </div>
+      <ea-social/>
+      <ui-menu class="ea-menu" />
+      
+    </header>
+    <transition name="intro" mode="out-in" appear>
+      <router-view :key="$route.fullPath"></router-view>
+    </transition>
+    
+    <ui-footer/>
+  </div>
+</template>
+<script>
+  import $ from 'jquery'
+  import uiLoading from '~/components/ui/loading.vue'
+  import uiLogo from '~/components/ui/logo.vue'
+  import uiMenu from '~/components/ui/menu.vue'
+  import uiFooter from '~/components/ui/footer.vue'
+  import eaSocial from '~/components/social.vue'
+  export default {
+      components: {
+          uiLoading,
+          uiLogo,
+          uiMenu,
+          uiFooter,
+          eaSocial
+      },
+      mounted () {
+      }
+  }
+</script>
