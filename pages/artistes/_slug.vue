11bb85f5c0410ae8891c3597e7b2fe255bd18e87
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-26 00:11:49 +0200

    Update _slug.vue

--- pages/artistes/_slug.vue
+++ pages/artistes/_slug.vue
@@ -1,12 +1,35 @@
 <template>
   <main class="ea-layout-lieu">
+    
     <h2 class="ea-titre">{{title}}</h2>
-    <article class="ea-article-artistes">
-      <div class="artistes-item__image" :style="{ backgroundImage: `url(${thumbnail})` }"></div>
-      <vue-markdown class="description">{{description}}</vue-markdown>
+    
+    <div class="lieux-item__image" :style="{ backgroundImage: `url(${thumbnail})` }"></div>
+        
+    <div class="lieu">        
+        <div class="logo-maps"><i class="fas fa-map-marked-alt"></i><p>{{ label }}</p></div>
+        <div class="logo-walk"><i class="fas fa-walking"></i><p>{{ langue }}</p></div>
+    </div>
+    
+    <article class="ea-article-lieu">
       <vue-markdown class="iframe">{{iframe}}</vue-markdown>
-      </div>
+      <vue-markdown class="description">{{description}}</vue-markdown>
+      
+      <div class="web-site" >
+        <a target="_blank"  v-for="s in social" :href="s.lien" :alt="s.texte">{{s.texte}}</a>
+      </div>      
     </article>
+    
+    <p class="title-grid">PROGRAMME</p>
+    <div class="grid">        
+        <div v-for="e in Evenement" :key="e.adresse" class="element-item">
+          <nuxt-link :to="'/programmation/soiree-'+ e.adresse +'/'" >
+             <p class="grid-date"><strong>{{e.dateh}}</strong></p>
+             <div class="line"></div>
+             <p class="grid-artistes"><strong>{{e.titr}}</strong></p>
+          </nuxt-link>
+        </div>
+    </div>
+    
   </main>
 </template>
 <script>
@@ -14,12 +37,12 @@ import $ from 'jquery'
 import VueMarkdown from 'vue-markdown'
 import VueLazyload from 'vue-lazyload'
 export default {
-  layout: 'default',
+  layout: 'simple',
   transition: { name: 'intro', mode: 'out-in' },
   components: { VueMarkdown, VueLazyload},
   async asyncData({ params }) {
-    let artistes = await import('~/content/artistes/page/' + params.slug + '.json');
-    return artistes;
+    let page = await import('~/content/artistes/page/' + params.slug + '.json');
+    return page;
   },
   head() {
     return {
@@ -32,23 +55,70 @@ export default {
       ]
     }
   },
-  updated() {
-  },
-  beforeMount(){
-  },
   mounted() {
-  },
-  destroyed() {
+    this.eal();
   },
   methods: {
-    
+    eal() {
+            var grid = new Isotope(".grid", {
+              itemSelector: ".element-item"
+            });
+            // store filter for each group
+            var filters = {};
+            $('#filters').on( 'click', '.button', function( event ) {
+                var $button = $( event.currentTarget );
+                // get group key
+                var $buttonGroup = $button.parents('.button-group');
+                var filterGroup = $buttonGroup.attr('data-filter-group');
+                // set filter for group
+                filters[ filterGroup ] = $button.attr('data-filter');
+                // combine filters
+                var filterValue = concatValues( filters );
+                // set filter for Isotope
+                grid.arrange({ filter: filterValue });
+            });
+            // change is-checked class on buttons
+            $('.button-group').each( function( i, buttonGroup ) {
+                var $buttonGroup = $( buttonGroup );
+                $buttonGroup.on( 'click', 'button', function( event ) {
+                  $buttonGroup.find('.is-checked').removeClass('is-checked');
+                  var $button = $( event.currentTarget );
+                  $button.addClass('is-checked');
+                });
+            });
+            $('.button-group').each( function( i, buttonGroup ) {
+                var $buttonGroup = $( buttonGroup );
+                $buttonGroup.on( 'click', '.button', function( event ) {
+                  $buttonGroup.find('.is-checked').removeClass('is-checked');
+                  var $button = $( event.currentTarget );
+                  $button.addClass('is-checked');
+                });
+            });
+            $( ".button-type .button" ).click(function() {
+              $( ".button-semaine .any" ).click();
+              $('.button-semaine .button').each( function() {
+                  $( this ).addClass( "active" );
+                  $( ".button-type" ).removeClass( "mobile-active" );
+              });
+            });
+            $( ".button-semaine .button" ).click(function() {
+              $('.button-semaine .button').each( function() {
+                  $( this ).removeClass( "active" );
+                  $( ".button-semaine" ).removeClass( "mobile-active" );
+              });
+            });
+            
+            
+            // flatten object by concatting values
+            function concatValues( obj ) {
+                var value = '';
+                for ( var prop in obj ) {
+                  value += obj[ prop ];
+                }
+                return value;
+            }
+            grid.layout();
+    }
   }
 };
 </script>
-<style>
-.ea-article-artistes .artistes-item__image {
-    height: 280px;
-    background-position: center center;
-    background-size: cover;
-}
-</style>
