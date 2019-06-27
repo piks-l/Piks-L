daa62b2dcdc4bf88ee9d2af1006da3c154e5e999
Zymeth <Zymeth@users.noreply.github.com>
2019-06-27 17:17:46 +0200

    Update _slug.vue

--- pages/film/_slug.vue
+++ pages/film/_slug.vue
@@ -72,48 +72,53 @@
     },
     mounted() {
 
-      // Open the Modal
-      function openModal() {
-        $("#myModal").css('display','block');
-      }
-
-      // Close the Modal
-      function closeModal() {
-        $("#myModal").css('display','none');
-      }
-
-      var slideIndex = 1;
-      showSlides(slideIndex);
-
-      // Next/previous controls
-      function plusSlides(n) {
-        showSlides(slideIndex += n);
-      }
-
-      // Thumbnail image controls
-      function currentSlide(n) {
-        showSlides(slideIndex = n);
-      }
-
-      function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("selected");
-        var captionText = document.getElementById("caption");
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";
-        }
-
-        slides[slideIndex-1].style.display = "block";
-        captionText.innerHTML = dots[slideIndex-1].alt;
-      }
+      this.diapo();
 
     },
     destroyed() {
     },
     methods: {
+
+      diapo(){
+        // Open the Modal
+        function openModal() {
+          $("#myModal").css('display','block');
+        }
+
+        // Close the Modal
+        function closeModal() {
+          $("#myModal").css('display','none');
+        }
+
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        // Next/previous controls
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+
+        // Thumbnail image controls
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          var dots = document.getElementsByClassName("selected");
+          var captionText = document.getElementById("caption");
+          if (n > slides.length) {slideIndex = 1}
+          if (n < 1) {slideIndex = slides.length}
+          for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+          }
+
+          slides[slideIndex-1].style.display = "block";
+          captionText.innerHTML = dots[slideIndex-1].alt;
+        }
+      }
+
     }
   };
 </script>
