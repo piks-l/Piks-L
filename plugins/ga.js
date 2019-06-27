216a26214f5d1265fa7869ab35e7f8dfa4459c87
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-06-27 12:44:47 +0200

    Update ga.js

--- plugins/ga.js
+++ plugins/ga.js
@@ -4,7 +4,7 @@ export default ({ app }) => {
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-120984751-1', 'auto')
+  ga('create', '', 'auto')
   app.router.afterEach((to, from) => {
     ga('set', 'page', to.fullPath)
     ga('send', 'pageview')
