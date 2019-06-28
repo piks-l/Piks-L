abdc48bd8feac38ebe890e281d6a9ada5f4615d3
Zymeth <Zymeth@users.noreply.github.com>
2019-06-28 15:14:38 +0200

    add factory

---
+++ pages/factory/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <main>
+    <div class="intro">
+        <p> La <b>Résidence 1+2 Factory</b> est une résidence artistique en entreprises ou en institutions, partenaires ou non du programme de la <b>Résidence 1+2
+          «Photographie &#38; Sciences»</b>. Sur une période donnée, un ou une photographe propose un regard d’auteur inédit sur la structure concernée en revisitant
+          son histoire, ses modes opératoires, sa production, ses savoirs-faire. Celle-ci apparaît alors, comme un soutien actif de la création photographique,
+          renforçant son ancrage local, régional ou national et s’ouvrant à d’autres publics. La Résidence 1+2 a pour ambition de créer une pépinière de jeunes
+          photographes et un réseau d’entreprises et institutions, sensibles aux expressions artistiques.</p>
+    </div>
+    <article class="small-article">
+        <a class="article-padding" href="article-film.html">
+            <img class="cover" src="">
+            <div class="content">
+                <h3 class="title-article">Les jours - cahiers photographiques</h3>
+                <hr>
+                <p class="description-article">Résidence 1+2 2017</p>
+                <small class="date">2017</small>
+            </div>
+        </a>
+    </article>
+  </main>
+</template>
+<script>
+  import $ from 'jquery'
+  import VueLazyload from 'vue-lazyload'
+  // export
+export default {
+    layout: 'default',
+    components: { VueLazyload },
+    transition: { name: 'intro', mode: 'out-in' },
+    head() {
+      return {
+        title: 'Programmation | Electrobotik Invasion - le 2 & 3 Août 2019',
+        meta: [
+          { hid: 'description', name: 'description', content: 'Electrobotik Invasion Festival, le 2 & 3 Août 2019, Circuit Paul Ricard - Le Castelet (83).' },
+          { 'property': 'og:title', 'content': 'Electrobotik Invasion - le 2 & 3 Août 2019', 'vmid': 'og:title' },
+          { 'property': 'og:description', 'content': 'Electrobotik Invasion Festival, le 2 & 3 Août 2019, Circuit Paul Ricard - Le Castelet (83).' },
+          { 'property': 'og:image', 'content': 'images/uploads/link_share.jpg', 'vmid': 'og:image' }
+        ]
+      }
+    },
+    data() {
+      const context = require.context('~/content/film/page/', false, /\.json$/);
+      const factory = context.keys().map(key => ({
+        ...context(key),
+        _path: `/factory/${key.replace('.json', '').replace('./', '')}`
+      }));
+      return {
+        factory
+      };
+    },
+  mounted() {
+  },
+  destroyed() {
+  },
+  methods: {
+  }
+}
+</script>
---
+++ pages/factory/slug.vue
@@ -0,0 +1,139 @@
+<template>
+  <main class="1plus2-layout-film">
+    <div class="left-side">
+        <div class="diapo">
+            <div v-for="i in galerie" class="image">
+              <img class="selected" :src="i.image" :alt="i.alt">
+            </div>
+        </div>
+        <div id="myModal" class="modal">
+            <span class="close-modal cursor">&times;</span>
+            <div class="modal-content">
+
+                <div v-for="i in galerie" class="mySlides">
+                    <img :src="i.image" :alt="i.alt">
+                </div>
+
+                <a class="prev">&#10094;</a>
+                <a class="next">&#10095;</a>
+
+            </div>
+            <div class="caption-container">
+                <p id="caption"></p>
+            </div>
+        </div>
+    </div>
+    <div class="right-side">
+        <small class="date">{{ date }}</small>
+        <h3 class="title-article">{{ title }}</h3>
+        <p class="description-article">{{ soustitre }}</p>
+        <div class="content">
+            <vue-markdown>{{ description }}</vue-markdown>
+            <br>
+            <vue-markdown>{{ article }}</vue-markdown>
+            <br>
+            <p class="read-more">POUR EN SAVOIR PLUS</p>
+            <a href="#" class="link">Résidence 2017</a>
+            <p class="no-margin">LIENS &#62;</p>
+            <a href="#" class="more-link">Clémentine Carrié</a>
+            <a href="#" class="more-link">Augustin Charnet</a>
+        </div>
+
+    </div>
+  </main>
+</template>
+<script>
+  import $ from 'jquery'
+  import VueMarkdown from 'vue-markdown'
+  import VueLazyload from 'vue-lazyload'
+  export default {
+    layout: 'default',
+    transition: { name: 'intro', mode: 'out-in' },
+    components: { VueMarkdown, VueLazyload },
+    async asyncData({ params }) {
+      let page = await import('~/content/factory/page/' + params.slug + '.json');
+      return page;
+    },
+    data() {
+      return {
+        slideIndex: 1
+      }
+    },
+
+    head() {
+      return {
+        title: '1+2 – Photographie & Sciences | ' +this.title,
+        meta: [
+          { hid: 'description', name: 'description', content: `${this.descriptionseo}` },
+          { 'property': 'og:title', 'content': `${this.title}`, 'vmid': 'og:titre' },
+          { 'property': 'og:description', 'content': `${this.descriptionseo}` },
+          { 'property': 'og:image', 'content': `${this.thumbnail}`, 'vmid': 'og:image' }
+        ]
+      }
+    },
+    updated() {
+
+    },
+    beforeMount(){
+
+    },
+    mounted() {
+
+      this.test();
+
+
+    },
+    destroyed() {
+    },
+    methods: {
+
+        test() {
+          var count=0;
+          var slideIndex = 1;
+          console.log(slideIndex);
+
+
+          $('.image').each( function( ) {
+              count += 1;
+              $(this).find( "img" ).attr('data-slide', count);
+          });
+
+          $('.close-modal').on( 'click', function() {
+              $("#myModal").css('display','none');
+          });
+          $('.image').on( 'click', function() {
+              $("#myModal").css('display','block');
+              slideIndex = $(this).find( "img" ).attr("data-slide");
+              showSlides(slideIndex);
+          });
+
+          $('.prev').on( 'click', function() {
+              showSlides(slideIndex += -1);
+
+          });
+          $('.next').on( 'click', function() {
+              showSlides(slideIndex += 1);
+          });
+
+
+          function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("selected");
+            var captionText = document.getElementById("caption");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";
+            }
+
+            slides[slideIndex-1].style.display = "block";
+            captionText.innerHTML = dots[slideIndex-1].alt;
+          }
+
+        },
+
+
+    }
+  };
+</script>
