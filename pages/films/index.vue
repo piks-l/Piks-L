3bf40bbe41f6e486c27d5e6b5747236cd866cfc9
Zymeth <Zymeth@users.noreply.github.com>
2019-07-09 13:12:11 +0200

    Update index.vue

--- pages/films/index.vue
+++ pages/films/index.vue
@@ -42,7 +42,7 @@ export default {
       };
     },
   mounted() {
-      $("body").removeClass('red-page, yellow-page, blue-page');
+      $("body").removeClass('red-page yellow-page blue-page');
       this.titre();
   },
   destroyed() {
