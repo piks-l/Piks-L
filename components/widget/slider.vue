3814bb321821311db2d689d30ec12eea4f828edc
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-07-17 16:47:58 +0200

    Update slider.vue

--- components/widget/slider.vue
+++ components/widget/slider.vue
@@ -1,5 +1,5 @@
 <template>
-  <carousel :autoplay="true" :nav="false" :loop="true" :items="4">
+  <carousel :autoplay="true" :nav="false">
     <img src="https://www.piks-l.com/img/5.jpg">
     <img src="https://www.piks-l.com/img/4.jpg">
     <img src="https://www.piks-l.com/img/3.jpg">
