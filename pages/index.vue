ce15b9a875d30ebe2f5d9f6c1d7bd5e3dffdd32f
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-07-17 17:18:43 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
 <template>
   <main class="page-index">
+    <h2>Typohraphie</h2>
+    <h1>Je suis un titre h1</h1>
+    <h2>Je suis un titre h2</h2>
+    <h3>Je suis un titre h3</h3>
+    <h4>Je suis un titre h4</h4>
+    <h5>Je suis un titre h5</h5>
+    <h6>Je suis un titre h6</h6>
+    <p>Je suis un P et lui c'est <strong>un Strong<strong>, quand à lui c'est un <a href="#">lien</a> et alors lui c'est juste un petit <small>small</small></p>
+    <hr>
     <h2>WIDGET</h2>
     <h3>Accordeon.vue</h3>
     <accordeon/>
+    <hr>
     <h3>Lightbox.vue</h3>
     <lightbox/>
+    <hr>
     <h3>Slider.vue</h3>
     <slider/>
+    <hr>
   </main>
 </template>
 <script>
