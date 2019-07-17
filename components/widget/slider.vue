4653c3d88466f6e5da3613944bab9a8e8f6fb058
Puxian AlHazred <50371128+PuxianAlHazred@users.noreply.github.com>
2019-07-17 16:31:03 +0200

    Update slider.vue

--- components/widget/slider.vue
+++ components/widget/slider.vue
@@ -1,8 +1,9 @@
 <template>
-  <carousel :autoplay="true" :nav="false">
-    <img src="https://www.piks-l.com/img/crop.jpg">
+  <carousel :autoplay="true" :nav="false" :loop="true" :items="4">
     <img src="https://www.piks-l.com/img/5.jpg">
     <img src="https://www.piks-l.com/img/4.jpg">
     <img src="https://www.piks-l.com/img/3.jpg">
+    <img src="https://www.piks-l.com/img/2.jpg">
+    <img src="https://www.piks-l.com/img/1.jpg">
   </carousel>
 </template>
